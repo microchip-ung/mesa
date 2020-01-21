239ca96af1050258dc98c1603635310e5e5edfc5
Claus Jensen <claus.jensen@microchip.com>
2020-01-21 12:31:54 +0000

    LAN966X CIL started

---
+++ base/lan966x/vtss_lan966x.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+// SPDX-License-Identifier: MIT
+
+
+#ifndef _VTSS_LAN966X_H_
+#define _VTSS_LAN966X_H_
+
+vtss_rc vtss_lan966x_inst_create(struct vtss_state_s *vtss_state);
+
+#endif /* _VTSS_LAN966X_H_ */
