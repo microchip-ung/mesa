4625c1b58acc529a8ceaad701bfde8f17a1a8196
Henrik Bjoernlund <henrik.bjoernlund@microchip.com>
2020-04-01 06:31:26 +0000

    The run_test_suites is multi threaded

--- mesa/demo/test/run_test_suites.rb
+++ mesa/demo/test/run_test_suites.rb
@@ -14,19 +14,13 @@ $systems = [
             { name: "dk-t34-4", image: "arm64/fit_vsc7558TSN.itb",       branch:"master" },
             { name: "dk-t34-3", image: "arm64/fit_vsc7558TSN.itb",       branch:"master" },
             { name: "dk-t12",   image: "mipsel/mfi_vsc7418.mfi",         branch:"master" },
-#            { name: "dk-t14",  image: "mipsel/mfi_vsc7468_48.mfi", branch:"master" }, MESA-428 / Atom issue
+            { name: "dk-t14",  image: "mipsel/mfi_vsc7468_48.mfi", branch:"master" }, MESA-428 / Atom issue
             { name: "dk-t15",   image: "mipsel/mfi_vsc7437.mfi",         branch:"master" },
-#            { name: "dk-t16",   image: "mipsel/mfi_vsc7429.mfi",   branch:"master" } Removed due to Ref-board instability (hangs)
+            { name: "dk-t16",   image: "mipsel/mfi_vsc7429.mfi",   branch:"master" } Removed due to Ref-board instability (hangs)
             { name: "dk-t34-1",  image: "lan966x/fit_ls1046_lan9668.itb", branch:"master.mas" }
            ]
 
-$test_suites  = ["suite_port.rb",
-                 "suite_l2_basic.rb",
-                 "suite_qos.rb",
-                 "suite_vop.rb",
-                 "suite_ts.rb",
-                 "suite_l3_rt.rb",
-                 "suite_examples.rb"]
+$test_suites  = "suite_port.rb suite_l2_basic.rb suite_qos.rb suite_vop.rb suite_ts.rb suite_l3_rt.rb suite_examples.rb"
 
 if File.file?("../../../../easytest/test-setup-server/et")
     $et = "../../../../easytest/test-setup-server/et"
@@ -35,40 +29,14 @@ else
 end
 
 $suites_log = []
-skip_system = []
-run_system = []
-
-def run_(cmd, verify=true)
-    puts "Executing #{cmd}"
-    res = system cmd
-    if verify
-        raise "Running '#{cmd}' failed" if $? != 0
-    end
-    res
-end
-
-
-def dl_file uri, out
-    FileUtils.mkdir_p File.dirname(out)
-    puts "Downloading #{uri} to #{out}"
-    IO.write(out, Net::HTTP.get(URI(uri)))
-end
-
-def run_suites(t)
-    $test_suites.each { |name|
-        $suites_log << "#{t}.#{name}.log"
-        res = run_("./#{name} > #{t}.#{name}.log", false)
-        if res
-            puts "Failed running #{name}"
-        end
-    }
-end
 
 def suite_log_merge name
     l = File.open(name,"w")
     found_start = false
     test_end = ""
-    $suites_log.each { |log|
+
+    suites_log = Dir.glob("*.log")
+    suites_log.each { |log|
         puts "Merge '#{log}'..."
         File.open(log).each do |line|
             if found_start
@@ -93,69 +61,24 @@ def suite_log_merge name
     puts "Merged to file '#{name}'"
 end
 
-# Release the system
-if (File.file?(".mscc-libeasy-topology.yaml"))
-    puts ".mscc-libeasy-topology.yaml Released"
-    run_("#{$et} -l release", false)
-elsif (File.file?("#{ENV['HOME']}/.mscc-libeasy-topology.yaml"))
-    run_("#{$et} release", false)
-end
+puts "-----delete all .log files-----"
+system("rm *.log")
 
+puts "-----Start test on all systems in background-----"
+Thread.new {`echo hi`}
 
+threads = []
 $systems.each { |system|
-    # Compose path to the image and test folder of the requested branch
-    jenkins_images = "http://soft00.microsemi.net:8080/job/API-mesa/job/" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
-
-    # Reserve system
-    res = run_("#{$et} -l reserve #{system[:name]}", false)
-    if !res
-        skip_system << system[:name]
-        next
-    end
-    run_system << system[:name]
-
-    # Download latest image from jenkins
-    dl_file "#{jenkins_images}/#{system[:image]}", "img"
-    # Download latest test folder from jenkins
-    dl_file "#{jenkins_images}/et.tar.gz", "et.tar.gz"
-
-    # Upload image to DUT
-    run_("#{$et} upload img")
-
-    # Unpack Easy Test folder tar file
-    run_("rm -rf test")
-    run_("tar xzf et.tar.gz")
-
-    # Copy the reserved system topology file to the created test folder
-    run_("cp .mscc-libeasy-topology.yaml test")
-
-    begin
-        # Change directory to the created test folder
-        cur = Dir.pwd
-        Dir.chdir "./test"
-        # Run test suites on system.
-        run_suites system[:name]
-
-        # Move .log files from created test folder to test folder
-        run_("mv *.log ../.")
-
-    rescue
-        run_("echo script #{system[:name]} failed")
-
-    ensure
-        Dir.chdir cur
-
-        run_("date")
-
-        # Release system
-        run_("#{$et} release")
-
-        run_("date")
-    end
-
+     threads << Thread.new {system "./utils/run-on.rb --system #{system[:name]} --image #{system[:image]} --branch #{system[:branch]} #{$test_suites}"}
 }
-puts "\nTested on systems:  #{run_system.join(" ")}"
-puts "Skipped systems:    #{skip_system.join(" ")}"
-puts "Merge the test-suite logs to one:"
-suite_log_merge "suite_log_merged.log"
+
+puts "-----Wait for all tests to complete-----"
+#threads.each(&:join)
+threads.each do |t|
+  t.join
+end
+puts "-----All tests are completed-----"
+
+puts("-----Merge the test-suite logs to one-----")
+suite_log_merge("suite_log_merged.log")
 
--- mesa/demo/test/utils/run-on.rb
+++ mesa/demo/test/utils/run-on.rb
@@ -8,30 +8,17 @@ require 'bundler/setup'
 require 'pp'
 require 'open3'
 require 'optparse'
+require 'fileutils'
+require 'net/http'
+
 
 $options = {}
 OptionParser.new do |opts|
-    opts.banner = "Usage: run-on.rb [options]"
+    opts.banner = "Usage: run-on.rb [options] -- list of tests"
 
     opts.on("-h", "--help", "This message") do |v|
         puts opts
         exit
-    end
-
-    opts.on("-d", "--dir dir", "Change directory to dir before doing anything.") do |d|
-        $options[:dir] = d
-    end
-
-    opts.on("-c", "--cmd cmd", "Command to run after reserving a test system") do |c|
-        $options[:cmd] = f
-    end
-
-    opts.on("-t", "--timeout seconds", "Keep trying to reserve the needed test system, until success or timeout") do |t|
-        $options[:timeout] = t
-    end
-
-    opts.on("-t", "--timeout seconds", "Keep trying to reserve the needed test system, until success or timeout") do |t|
-        $options[:timeout] = t
     end
 
     opts.on("-s", "--system system", "System to reserve") do |s|
@@ -39,13 +26,114 @@ OptionParser.new do |opts|
     end
 
     opts.on("-i", "--image image", "Image to upload") do |i|
-        $options[:img] = i
+        $options[:image] = i
     end
 
-    opts.on("-o", "--out output", "Where to write the output of the command") do |o|
-        $options[:out] = i
+    opts.on("-b", "--branch branch", "branch to upload") do |b|
+        $options[:branch] = b
+    end
+
+    opts.on("-t", "--timeout seconds", "Keep trying to reserve the needed test system, until success or timeout") do |t|
+        $options[:timeout] = t
     end
 
 end.parse!
 
+if File.file?("../../../../easytest/test-setup-server/et")
+    $et = "../../../../easytest/test-setup-server/et"
+else
+    $et = "/easytest/easytest/test-setup-server/et"
+end
 
+def dl_file uri, out
+    FileUtils.mkdir_p File.dirname(out)
+    puts "Downloading #{uri} to #{out}"
+    IO.write(out, Net::HTTP.get(URI(uri)))
+end
+
+def run_(cmd, verify=true)
+    puts "Executing #{cmd}"
+    res = system cmd
+    if verify
+        raise "Running '#{cmd}' failed" if $? != 0
+    end
+    res
+end
+
+def run_suites(system)
+    ARGV.each { |suite|
+        puts("run suite #{suite} on system #{system}")
+        res = run_("./#{suite} > #{system}.#{suite}.log", false)
+        if !res
+            puts "Failed running suite #{suite} on system #{system}"
+        end
+    }
+end
+
+if ($options[:system] != nil) && ($options[:image] != nil) && ($options[:branch] != nil)
+    system = $options[:system]
+
+    # Compose path to the image and test folder of the requested branch
+    jenkins_images = "http://soft00.microsemi.net:8080/job/API-mesa/job/" + $options[:branch] + "/lastSuccessfulBuild/artifact/images"
+
+    # Reserve system
+    t1 = Time.now
+    seconds = ($options[:timeout] != nil) ? $options[:timeout] : 0
+    puts("Try reserve system #{system}");
+    loop do
+        res = run_("#{$et} -l -n #{system} reserve #{system}", false)
+        if res
+            break   # break on sucess
+        end
+
+        if ((Time.now - t1) > seconds)
+            exit 7  # timeout - return with error
+        end
+
+        sleep(30)   # sleep and try again
+        next
+    end
+    puts("Reservation of #{system} successful");
+
+    puts("Download latest image from jenkins");
+    dl_file "#{jenkins_images}/#{$options[:image]}", "#{system}-img"
+    puts("Download latest test folder from jenkins")
+    dl_file "#{jenkins_images}/et.tar.gz", "#{system}-et.tar.gz"
+
+    puts("Upload image to DUT")
+    run_("#{$et} -l -n #{system} upload #{system}-img")
+
+    puts("Unpack Easy Test folder tar file")
+    run_("rm -rf #{system}-test")
+    run_("mkdir #{system}-test")
+    run_("tar xzf et.tar.gz -C #{system}-test")
+
+    puts("Move the reserved system topology file to the created test folder")
+    run_("mv .mscc-libeasy-topology#{system}.yaml ./#{system}-test/test/.mscc-libeasy-topology.yaml")
+
+    begin
+        run_("date")
+
+        # Change directory to the created test folder
+        cur = Dir.pwd
+        Dir.chdir "./#{system}-test/test"
+
+        puts("Run test suites on system #{system}.")
+        run_suites system
+
+        # Move .log files from created test folder to test folder
+        run_("mv *.log ../../.")
+
+    rescue
+        run_("Test on system #{system} failed")
+
+    ensure
+        # Release system
+        run_("#{$et} -l release")
+
+        run_("date")
+
+        Dir.chdir cur
+
+    end
+end
