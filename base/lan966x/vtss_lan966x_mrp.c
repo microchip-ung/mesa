fa39ff5c6dd4cff1dece2f7f8e8300e70ca54b35
Claus Jensen <claus.jensen@microchip.com>
2021-01-14 12:02:21 +0000

    Copyright fix

--- base/lan966x/vtss_lan966x_mrp.c
+++ base/lan966x/vtss_lan966x_mrp.c
@@ -1,5 +1,4 @@
 // Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
-// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
 // SPDX-License-Identifier: MIT
 
 
