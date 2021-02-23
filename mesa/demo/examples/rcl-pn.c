8af8fed5755f088bf40e9ed0130518a02b501912
Claus Jensen <claus.jensen@microchip.com>
2021-02-23 14:58:17 +0000

    Added Profinet example

---
+++ mesa/demo/examples/rcl-pn.c
@@ -0,0 +1,69 @@
+// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+// SPDX-License-Identifier: MIT
+
+#include <unistd.h>
+#include <stdio.h>
+#include "cli.h"
+#include "example.h"
+#include "microchip/ethernet/switch/api.h"
+
+static int rcl_pn_init(int argc, const char *argv[])
+{
+    mesa_port_no_t      iport = ARGV_INT("iport", "Ingress port");
+    mesa_rc             rc = MESA_RC_ERROR;
+    mesa_port_map_t     *port_map;
+    uint32_t            port_cnt = mesa_port_cnt(NULL);
+    mesa_port_no_t      port_no;
+    mesa_rcl_vid_conf_t conf;
+    int                 pcp;
+    mesa_rce_t          rce;
+
+    EXAMPLE_BARRIER(argc);
+
+    // Find port number mapping to RTE port
+    if ((port_map = calloc(port_cnt, sizeof(*port_map))) == NULL) {
+        return -1;
+    }
+    if (mesa_port_map_get(NULL, port_cnt, port_map) == MESA_RC_OK) {
+        for (port_no = 0; port_no < port_cnt; port_no++) {
+            if (port_map[port_no].chip_port == 4) {
+                rc = MESA_RC_OK;
+                break;
+            }
+        }
+    }
+    free(port_map);
+    RC(rc);
+
+    // Enable untagged RTP processing
+    for (pcp = 0; pcp < MESA_PCP_CNT; pcp++) {
+        conf.pcp[pcp] = 1;
+    }
+    RC(mesa_rcl_vid_add(NULL, 0, &conf));
+
+    // Add RCE mapping to RTP ID
+    RC(mesa_rce_init(NULL, &rce));
+    rce.id = 1;
+    rce.key.port_no = iport;
+    rce.key.etype = MESA_RCL_ETYPE_PROFINET;
+    rce.action.rtp_id = 1;
+    rce.action.port_enable = 1;
+    mesa_port_list_set(&rce.action.port_list, port_no, 1);
+    RC(mesa_rce_add(NULL, MESA_RCE_ID_LAST, &rce));
+
+    return 0;
+}
+
+static int rcl_pn_uninit(void)
+{
+    RC(mesa_rcl_vid_del(NULL, 0));
+    RC(mesa_rce_del(NULL, 1));
+    return 0;
+}
+
+static const char *rcl_pn_help(void)
+{
+    return "RCL Profinet example\n";
+}
+
+EXAMPLE(pn, rcl_pn_init, NULL, rcl_pn_uninit, rcl_pn_help);
---
+++ mesa/demo/test/example_rte_pn.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+
+# Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+# SPDX-License-Identifier: MIT
+
+require_relative 'libeasy/et'
+
+$ts = get_test_setup("mesa_pc_b2b_4x")
+
+#---------- Configuration -----------------------------------------------------
+
+$idx_tx = 0 # PC Tx port
+$idx_rx = 1 # PC Rx port
+
+test "conf" do
+    # Initialize QSPI
+    qspi_init
+
+    # Clear IO memory
+    io_fpga_rw("fill 0x0100 0x100 0")
+
+    # Write data to QSPI
+    io_str_wr(0x110, "0504030201", "QSPI")
+
+    # Find chip Tx port
+    port_cnt = cap_get("PORT_CNT")
+    map = $ts.dut.call("mesa_port_map_get", port_cnt)
+    eport = map[$ts.dut.p[$idx_rx]]["chip_port"]
+
+    # Initialize MESA/MERA examples
+    $ts.dut.run("mesa-cmd example init pn iport #{$ts.dut.p[$idx_tx]}")
+    $ts.dut.run("mera-cmd example init pn eport #{eport}")
+end
+
+test "frame-io" do
+    # Frame f1 is sent to RTE-OB with incrementing data pattern
+    len = 46
+    f1 = "eth dmac 2 smac 1 et 0x8892 data hex 8000"
+    f1 += " data pattern cnt #{len - 8}"
+    cc = "ccc0"
+    ds = "35"
+    ts = "00"
+    f1 += " data hex #{cc}#{ds}#{ts}"
+
+    # Frame f2 is sent by RTE-IB every second with data "0504030201" at frame offset 2
+    f2 = "eth dmac 1 smac 2 et 0x8892 data hex 8000 data hex "
+    for i in 0..(len - 7) do
+        d = (i < 5 ? (5 - i) : 0)
+        f2 += ("%02x" % d)
+    end
+    f2 += " ctag pcp ign dei ign vid ign" # Ignore CC using C-tag
+
+    cmd = "sudo ef -t 900 name f1 #{f1} name f2 #{f2}"
+    cmd += " tx #{$ts.pc.p[$idx_tx]} name f1"
+    [0, 1, 2, 3].each do |idx|
+        cmd += " rx #{$ts.pc.p[idx]}"
+        if (idx == $idx_rx)
+            cmd += " name f2"
+        end
+    end
+    $ts.pc.run(cmd)
+
+    # Check data pattern read from frame offset 2.
+    addr = 0x100
+    str = "0001020304"
+    intf = "QSPI"
+    res = io_str_rd(addr, str, intf)
+    t_i("Exp (#{intf}): #{str}")
+    act = ("0x%08x: #{res}" % addr)
+    if (res == str)
+        t_i(act)
+    else
+        t_e(act)
+    end
+end
+
+test "uninit" do
+    $ts.dut.run("mesa-cmd example uninit")
+    $ts.dut.run("mera-cmd example uninit")
+end
+
+test "dump" do
+    break
+    $ts.dut.run("mera-cmd debug api ob")
+    io_fpga_rw("dump 0x0100 64")
+end
