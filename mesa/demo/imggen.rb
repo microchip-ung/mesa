d52ee9ca7002585eaada8c4a1fe93deaef6353cc
Horatiu Vultur <horatiu.vultur@microchip.com>
2021-06-23 21:03:18 +0200

    Bump BSP and update DT for pcb8309

--- mesa/demo/imggen.rb
+++ mesa/demo/imggen.rb
@@ -101,6 +101,11 @@ $machines = {
                 { :name => "6849_0@lan966x", :file => "#{$p_mesa}/meba/dt/meba_lan966x_6849_0.dtso"},
                 { :name => "8290_0@lan966x", :file => "#{$p_mesa}/meba/dt/meba_lan966x_8290_0.dtso"},
                 { :name => "8291_0@lan966x", :file => "#{$p_mesa}/meba/dt/meba_lan966x_8291_0.dtso"},
+            ]
+          },
+          {
+            :file => "#{$bsp}/arm-cortex_a8-linux-gnu/xstax/release/lan966x-mesa-pcb8309.dtb",
+            :overlays => [
                 { :name => "8309_0@lan966x", :file => "#{$p_mesa}/meba/dt/meba_lan966x_8309_0.dtso"},
             ]
           },
