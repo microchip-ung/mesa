bdbed5c9bc04f8649127f33e2250c5d20a4c8572
Claus Jensen <claus.jensen@microchip.com>
2021-08-19 07:13:21 +0000

    Copyright update for PHY build files

--- phy_demo_appl/build_venice_cli_sw_char.sh
+++ phy_demo_appl/build_venice_cli_sw_char.sh
@@ -1,27 +1,6 @@
-#
-#
-# Copyright (c) 2002-2017 Microsemi Corporation "Microsemi". All Rights Reserved.
-#
-# Unpublished rights reserved under the copyright laws of the United States of
-# America, other countries and international treaties. Permission to use, copy,
-# store and modify, the software and its source code is granted but only in
-# connection with products utilizing the Microsemi switch and PHY products.
-# Permission is also granted for you to integrate into other products, disclose,
-# transmit and distribute the software only in an absolute machine readable format
-# (e.g. HEX file) and only in or with products utilizing the Microsemi switch and
-# PHY products.  The source code of the software may not be disclosed, transmitted
-# or distributed without the prior written permission of Microsemi.
-#
-# This copyright notice must appear in any copy, modification, disclosure,
-# transmission or distribution of the software.  Microsemi retains all ownership,
-# copyright, trade secret and proprietary rights in the software and its source code,
-# including all modifications thereto.
-#
-# THIS SOFTWARE HAS BEEN PROVIDED "AS IS". MICROSEMI HEREBY DISCLAIMS ALL WARRANTIES
-# OF ANY KIND WITH RESPECT TO THE SOFTWARE, WHETHER SUCH WARRANTIES ARE EXPRESS,
-# IMPLIED, STATUTORY OR OTHERWISE INCLUDING, WITHOUT LIMITATION, WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR USE OR PURPOSE AND NON-INFRINGEMENT.
-#
+# Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+# SPDX-License-Identifier: MIT
+
 ##    -DRPI_MIIM \
 ##    -DVTSS_FEATURE_PHY_TIMESTAMP \
 ##    -DVTSS_ARCH_VENICE_C=TRUE \
