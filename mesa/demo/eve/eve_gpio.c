58eb634a1c35869c3de0b593cd5bc0e0067dde5b
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2021-09-27 10:23:02 +0200

    Fix compile issue

--- mesa/demo/eve/eve_gpio.c
+++ mesa/demo/eve/eve_gpio.c
@@ -1,5 +1,5 @@
-# Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
-# SPDX-License-Identifier: MIT
+// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+// SPDX-License-Identifier: MIT
 
 /*
  * eve_gpio.c
