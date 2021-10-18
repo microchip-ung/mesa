360f81ffe38fae88d53fcbe8f31353aa3c2944dc
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2021-10-18 10:23:47 +0200

    Added 'meba' to tags

--- .cmake/tags.cmake
+++ .cmake/tags.cmake
@@ -1,6 +1,6 @@
 # Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
 # SPDX-License-Identifier: MIT
 
-add_custom_target(tags etags `find ./include/ ./base/ ./mesa/ ./linux_support/  -name \".*\" -prune -o -type f -print`
+add_custom_target(tags etags `find ./include/ ./base/ ./mesa/ ./linux_support/ ./meba/  -name \".*\" -prune -o -type f -print`
                   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} COMMENT "Generate tag file for Emacs...")
 
