b933daeaf0c716e8b1ffd3268d36eb879e94ede4
Rene Nielsen <rene.nielsen@microchip.com>
2021-10-26 11:27:52 +0200

    Bumped release-utils because Jira maintainers changed 'In Progress' to 'InProgress'

--- .cmake/release_utils.rb
+++ .cmake/release_utils.rb
@@ -10,11 +10,10 @@
 # To clone the real stuff, do a
 # git clone ssh://git@bitbucket.microchip.com/unge/sw-tools-mscc-release-utils.git
 
-MSCC_RELEASE_UTILS_VER     = 'v0.136'
+MSCC_RELEASE_UTILS_VER     = 'v0.140'
 MSCC_RELEASE_UTILS_PACKAGE = 'mscc-sw-release-utils'
 MSCC_RELEASE_UTILS_SUBDIR  = "#{MSCC_RELEASE_UTILS_PACKAGE}-#{MSCC_RELEASE_UTILS_VER}"
 MSCC_RELEASE_UTILS_DIR     = "/opt/mscc/#{MSCC_RELEASE_UTILS_SUBDIR}"
-#MSCC_RELEASE_UTILS_DIR     = "/home/anielsen/work/sw-tools-mscc-release-utils"
 
 if !system("sudo /usr/local/bin/mscc-install-pkg -t #{MSCC_RELEASE_UTILS_PACKAGE} #{MSCC_RELEASE_UTILS_SUBDIR}")
     STDERR.puts("Error: Unable to install version #{MSCC_RELEASE_UTILS_VER} of #{MSCC_RELEASE_UTILS_PACKAGE}")
