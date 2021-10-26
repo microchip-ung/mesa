2cc04733f89b9a3882d85c4af992a8ac6cb72279
Claus Jensen <claus.jensen@microchip.com>
2021-10-26 07:34:41 +0000

    Luton10 test setup added

--- mesa/demo/test/run_test_suites.rb
+++ mesa/demo/test/run_test_suites.rb
@@ -26,7 +26,7 @@ $systems = [
     { name: "dk-t35-4", image: "armv7_lan966x_bbb.itb",      branch:"master", parallel: "no", server: "35", started: "no" },
     { name: "dk-t35-2", image: "mipsel_vsc7437.mfi",         branch:"master", parallel: "no", server: "35", started: "no" },
     { name: "dk-t35-1", image: "mipsel_vsc7514_pcb123.mfi",  branch:"master", parallel: "no", server: "35", started: "no" },
-    { name: "dk-t35-3", image: "mipsel_vsc7429.mfi",         branch:"master", parallel: "no", server: "35", started: "no" },
+    { name: "dk-t35-4", image: "mipsel_vsc7428.mfi",         branch:"master", parallel: "no", server: "35", started: "no" },
 
 #   { name: "dk-t31",   image: "arm64_vsc7546TSN.itb",       branch:"master", parallel: "no", server: "35", started: "no" },
 #   { name: "dk-t35-6", image: "mipsel_vsc7468_48.mfi",      branch:"master", parallel: "no", server: "35", started: "no" }, MESA-428 / Atom issue
