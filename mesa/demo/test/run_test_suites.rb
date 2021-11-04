dd52d5f253acbb427fb72a8fbff135e84c5c59de
Claus Jensen <claus.jensen@microchip.com>
2021-11-04 08:49:40 +0000

    Using dk-t33-5 instead of dk-t33-6 for Maserati 8-port test

--- mesa/demo/test/run_test_suites.rb
+++ mesa/demo/test/run_test_suites.rb
@@ -13,7 +13,7 @@ $systems = [
     { name: "dk-t33-0", image: "armv7_lan966x.itb",          branch:"master", parallel: "no", server: "33", started: "no" },
     { name: "dk-t33-2", image: "armv7_lan966x.itb",          branch:"master", parallel: "no", server: "33", started: "no" },
     { name: "dk-t33-4", image: "armv7_lan966x.itb",          branch:"master", parallel: "no", server: "33", started: "no" },
-    { name: "dk-t33-6", image: "armv7_lan966x.itb",          branch:"master", parallel: "no", server: "33", started: "no" },
+    { name: "dk-t33-5", image: "armv7_lan966x.itb",          branch:"master", parallel: "no", server: "33", started: "no" },
 
     # dk-t34, starting with Fireant systems
     { name: "dk-t34-3", image: "arm64_vsc7558TSN.itb",       branch:"master", parallel: "no", server: "34", started: "no" },
