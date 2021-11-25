485827cbabe72d3e58da427a18993258fb39bd62
Allan W. Nielsen <allan.nielsen@microchip.com>
2021-11-25 11:09:56 +0100

    Fix copyright

--- mepa/.cmake/mepa-macros.cmake
+++ mepa/.cmake/mepa-macros.cmake
@@ -1,3 +1,6 @@
+# Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+# SPDX-License-Identifier: MIT
+
 macro(MEPA_DRV)
     set(oneValueArgs   LIB_NAME)
     set(multiValueArgs SRCS DEFS INCL_PUB INCL_PRI)
