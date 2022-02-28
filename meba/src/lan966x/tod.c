a11e1e2bd9a0e073e9c26c4b933c2d54a949b18a
Sriharsha Sankuratripati <sriharsha.sankuratripati@microchip.com>
2022-02-28 01:21:05 +0000

    Bugzilla#None: Indy phy on maserati platform needs qsgmii recovered clock input

--- meba/src/lan966x/tod.c
+++ meba/src/lan966x/tod.c
@@ -15,7 +15,7 @@ mesa_rc meba_tod_phy_ts_clk_info(meba_inst_t inst,
 {
     // Indy phy config.
     *freq = MEPA_TS_CLOCK_FREQ_250M;
-    *src = MEPA_TS_CLOCK_SRC_EXT_1588_REF_CLOCK;
+    *src = MEPA_TS_CLOCK_SRC_125MHZ_QSGMII_REC_CLOCK;
     T_I(inst, "Maserati board 250Mhz 1pps latency of 2 clk cycles");
 
     /* default 1pps latency for the phy is 2 clock cycles (updated from ocelot) */
