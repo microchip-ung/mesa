6361d8a6347009e31b7c23ae90e1643695fbcbd4
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2022-04-07 11:44:02 +0200

    ET:Using master.laguna-12

--- mesa/demo/test/run_test_suites.rb
+++ mesa/demo/test/run_test_suites.rb
@@ -18,7 +18,7 @@ $systems = [
     # dk-t34, starting with Fireant systems
     { name: "dk-t34-3", image: "arm64_vsc7558TSN.itb",       branch:"master", parallel: "no", server: "34", started: "no" },
     { name: "dk-t34-4", image: "arm64_vsc7558TSN.itb",       branch:"master", parallel: "no", server: "34", started: "no" },
-    { name: "dk-t34-5", image: "arm64_vsc7558TSN.itb",       branch:"master.laguna-11", parallel: "no", server: "34", started: "no" },
+    { name: "dk-t34-5", image: "arm64_vsc7558TSN.itb",       branch:"master.laguna-12", parallel: "no", server: "34", started: "no" },
     { name: "dk-t34-0", image: "mipsel_vsc7468_pcb110.mfi",  branch:"master", parallel: "no", server: "34", started: "no" },
     { name: "dk-t34-2", image: "ls1046_vsc7512.itb",         branch:"master", parallel: "no", server: "34", started: "no" },
 
@@ -26,7 +26,7 @@ $systems = [
     { name: "dk-t35-2", image: "mipsel_vsc7437.mfi",         branch:"master", parallel: "no", server: "35", started: "no" },
     { name: "dk-t35-1", image: "mipsel_vsc7514_pcb123.mfi",  branch:"master", parallel: "no", server: "35", started: "no" },
     { name: "dk-t35-4", image: "mipsel_vsc7428.mfi",         branch:"master", parallel: "no", server: "35", started: "no" },
-    { name: "dk-t35-6", image: "armv7_lan969x_sr_bbb.itb",   branch:"master.laguna-11", parallel: "no", server: "35", started: "no" },
+    { name: "dk-t35-6", image: "armv7_lan969x_sr_bbb.itb",   branch:"master.laguna-12", parallel: "no", server: "35", started: "no" },
 
 #   { name: "dk-t31",   image: "arm64_vsc7546TSN.itb",       branch:"master", parallel: "no", server: "35", started: "no" },
 #   { name: "dk-t35-6", image: "mipsel_vsc7468_48.mfi",      branch:"master", parallel: "no", server: "35", started: "no" }, MESA-428 / Atom issue
