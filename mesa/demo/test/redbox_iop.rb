9b3a3348b8f9393bd11f04808bcff8dd0668fdc5
Claus Jensen <claus.jensen@microchip.com>
2022-05-19 11:21:35 +0000

    RedBox/IE4000 test now supports HSR-PRP

--- mesa/demo/test/redbox_iop.rb
+++ mesa/demo/test/redbox_iop.rb
@@ -15,18 +15,18 @@ $ts = get_test_setup("mesa_pc_b2b_4x")
 # |      |           |  Laguna  | Switch Core
 # |    1 |- G --- 1 -|          |
 # |      |           +----------+
-# |      |           |  RedBox  | HSR-SAN or PRP-SAN
+# |      |           |  RedBox  | HSR-SAN/PRP-SAN/HSR-PRP
 # |      |           +----------+
 # |      |              |    |
 # |      |              2    3
 # |  PC  |              |    |
-# |      |       Port A |    | Port B
+# |      |       Port A |    | Port B (down for HSR-PRP)
 # |      |              |    |
 # |      |              1    2
 # |      |              |    |
 # |      |           +----------+
 # |    2 |- B --- 3 -|          |
-# |      |           |  IE4000  | HSR-SAN or PRP-SAN
+# |      |           |  IE4000  | HSR-SAN/PRP-SAN/HSR-PRP
 # |    3 |- Y --- 4 -|          |
 # +------+           +----------+
 #    |                     |
@@ -40,13 +40,16 @@ $ts = get_test_setup("mesa_pc_b2b_4x")
 # HSR-VLAN: 'copy hsr-vlan-config startup-config' and 'reload'
 # PRP-SAN : 'copy prp-san-config startup-config' and 'reload'
 # PRP-VLAN: 'copy prp-vlan-config startup-config' and 'reload'
+# HSR-PRP : 'copy hsr-prp-config startup-config' and 'reload'
 
-# Before test, RedBox mode is selected
+# Laguna RedBox mode must match IE4000 RedBox mode
 $mode = "PRP_SAN"
 #$mode = "HSR_SAN"
+#mode = "HSR_PRP"
 
 # Select if VLAN tagging is used on LRE ports
 $vlan = false
+#$vlan = true
 
 test "conf" do
     if ($vlan)
@@ -70,25 +73,70 @@ test "conf" do
     rb_id = 0
     conf = $ts.dut.call("mesa_rb_conf_get", rb_id)
     conf["mode"] = ("MESA_RB_MODE_" + $mode)
-    conf["port_a"] = 2
-    conf["port_b"] = 3
+    if ($mode == "HSR_PRP")
+        conf["port_a"] = 0
+        conf["port_b"] = 1
+        conf["net_id"] = 6
+    else
+        conf["port_a"] = 2
+        conf["port_b"] = 3
+    end
     conf["dd_age_time"] = 10000
     conf["sv"] = "MESA_RB_SV_CPU_ONLY"
     $ts.dut.call("mesa_rb_conf_set", rb_id, conf)
 end
 
 test "frame-io" do
+    hsr_prp = ($mode == "HSR_PRP")
     idx_list = [0,1,2,3]
     tx_list = idx_list
     #tx_list = [2]
     tx_list.each do |idx_tx|
-        cmd = "sudo ef name f1 eth smac #{idx_tx + 1} et 0xaaaa data pattern cnt 46"
+        len = 46
+        f_base = " eth smac #{idx_tx + 1}"
+        f_end = " et 0xaaaa data pattern cnt #{len}"
+
+        # Frame f1 (Tx and Rx)
+        cmd = ("sudo ef name f1" + f_base)
+        seqn = 7
+        len += 6
+        if (hsr_prp)
+            cmd += " htag pathid 0 size #{len} seqn #{seqn}"
+        end
+        cmd += f_end
+
+        if (hsr_prp)
+            # Frame f2 (Rx)
+            cmd += (" name f2" + f_base)
+            if (idx_tx > 1)
+                # IE4000 -> Laguna: NetId 6 and PRP trailer not popped by Laguna.
+                netid = 6
+                prp = " prp seqn #{seqn} lanid 0xa size #{len}"
+                seqn = 1
+                len += 6
+            else
+                # Laguna -> IE4000: NetId 1 and PRP trailer popped by IE4000.
+                netid = 1
+                prp = ""
+            end
+            pathid = (netid << 1)
+            cmd += " htag pathid #{pathid} size #{len} seqn #{seqn}"
+            cmd += (f_end + prp)
+
+            # Frame f3 (Rx from IE4000 HSR port B)
+            pathid += 1
+            cmd += (" name f3" + f_base)
+            cmd += " htag pathid #{pathid} size #{len} seqn #{seqn}"
+            cmd += (f_end + prp)
+        end
+
         idx_list.each do |idx|
             name = $ts.pc.p[idx]
             if (idx == idx_tx)
                 cmd += " tx #{name} name f1"
             else
-                cmd += " rx #{name} name f1"
+                cmd += " rx #{name} name "
+                cmd += ((hsr_prp and (idx_tx/2) != (idx/2)) ? (idx == 3 ? " f3" : " f2") : "f1")
             end
         end
         $ts.pc.try(cmd)
