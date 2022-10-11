35413df8c1bc122966ae463a42888b872d46336f
Palle Christensen <palle.christensen@microchip.com>
2022-10-11 16:00:01 +0200

    Add ts_clk_info for PCB135 rev 3

--- meba/src/sparx5/tod.c
+++ meba/src/sparx5/tod.c
@@ -5,6 +5,7 @@
 #include "microchip/ethernet/board/api.h"
 
 #include "meba_aux.h"
+#include "meba_common.h"
 
 mesa_rc meba_tod_phy_ts_clk_info(meba_inst_t inst,
                                  mepa_port_no_t       port,
@@ -13,8 +14,16 @@ mesa_rc meba_tod_phy_ts_clk_info(meba_inst_t inst,
                                  mepa_timeinterval_t *pps_load_delay,
                                  mepa_timeinterval_t *pps_save_delay)
 {
-    T_I(inst, "TBD");
-    return MESA_RC_NOT_IMPLEMENTED;
+    // Will only be called for PCB135 version 3 with Indy Phy. Do Indy phy config. To be verified
+    *freq = MEPA_TS_CLOCK_FREQ_250M;
+    *src = MEPA_TS_CLOCK_SRC_125MHZ_QSGMII_REC_CLOCK;
+    T_I(inst, "PCB135 board with Indy phy");
+
+    /* default 1pps latency for the phy is 2 clock cycles (updated from ocelot) */
+    *pps_load_delay = (((mesa_timeinterval_t)MESA_ONE_MIA << 16) * 3LL) / clk_mhz[*freq];
+    *pps_save_delay = (((mesa_timeinterval_t)MESA_ONE_MIA << 16) * 2LL) / clk_mhz[*freq];
+
+    return MESA_RC_OK;
 }
 
 static meba_api_tod_t public_functions = {
