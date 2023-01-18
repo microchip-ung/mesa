eed6d2a1854d336346ab232de809305c26a2df5f
Henrik Bjoernlund <henrik.bjoernlund@microchip.com>
2023-01-18 09:50:46 +0100

    Updated easytest scripts to make clean Allure report

--- mesa/demo/test/qos-scheduler.rb
+++ mesa/demo/test/qos-scheduler.rb
@@ -96,7 +96,7 @@ test "Strict scheduling test from #{ig_list} to #{$ts.dut.p[eg]}" do
         measure(ig, eg, 1000, 1,     false,            false,           [0,0,990000000],  [295,535,2],  true,              [0,3,7]) # On SparX-5 some lower priority frames are slipping through
     else
     if ($chip_family == chip_family_to_id("MESA_CHIP_FAMILY_LAN966X"))
-        measure(ig, eg, 1000, 1,     false,            false,           [0,0,990000000],  [210,500,0.3], true,             [0,3,7]) # On LAN966X some lower priority frames are slipping through
+        measure(ig, eg, 1000, 1,     false,            false,           [0,0,990000000],  [260,500,0.3], true,             [0,3,7]) # On LAN966X some lower priority frames are slipping through
     else
     if ($chip_family == chip_family_to_id("MESA_CHIP_FAMILY_OCELOT"))
         measure(ig, eg, 1000, 1,     false,            false,           [0,0,990000000],  [340,380,2],  true,              [0,3,7]) # On Ocelot some lower priority frames are slipping through
