34ea48d4738a8134be166db872ba0d9cf6a2cf8a
ezra <ekoper@microsemi.com>
2023-08-01 09:03:28 +0300

    adding poe support to lan966x
    Review-record: https://bitbucket.microchip.com/projects/UNGE/repos/sw-mesa/pull-requests/346/

---
+++ meba/src/lan966x/lan966x_poe_hw_cfg.h
@@ -0,0 +1,281 @@
+// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+// SPDX-License-Identifier: MIT
+
+#ifndef _LAN966X_POE_HW_CFG_H_
+#define _LAN966X_POE_HW_CFG_H_
+
+#include <stdio.h>
+#include "microchip/ethernet/board/api.h"
+#include "meba_poe_generic.h"
+#include "poe_driver.h"
+
+// lan966x has both BT and AT implementations
+
+//--------------------------------------------------------------------------------------//
+//------------------------------    general parameters   -------------------------------//
+//--------------------------------------------------------------------------------------//
+
+
+// This firmware can be used with the PD692x0 family of PoE controllers (PD69210, PD69220, and PD69200).
+// please choose 'ePoE_Controller_Type_Unknown' for automatic firmware detection or force specific PD692x0 PoE controller
+//
+// MEBA_POE_PD692X0_CONTROLLER_TYPE_AUTO_DETECTION
+// MEBA_POE_PD69200_CONTROLLER_TYPE ,
+// MEBA_POE_PD69210_CONTROLLER_TYPE ,
+// MEBA_POE_PD69220_CONTROLLER_TYPE ,
+#define LAN9668_POE_PD692x0_CONTROLLER_TYPE_DEFAULT           MEBA_POE_PD692X0_CONTROLLER_TYPE_AUTO_DETECTION
+
+
+// Set system mode BT or AT firmware:
+// MEBA_POE_FIRMWARE_TYPE_AT - AF/AT mode
+// MEBA_POE_FIRMWARE_TYPE_BT - BT mode
+#define LAN9668_POE_FIRMWARE_TYPE_DEFAULT                      MEBA_POE_FIRMWARE_TYPE_BT
+
+
+// Set unit max power as fixed or variable through Web & CLI
+// User Conig 1=Yes,0=No
+#define LAN9668_POE_UNIT_MAX_POWER_USER_CONFIG_DEFAULT         0
+
+
+// Power Suply maximum Power (W)
+#define LAN9668_POE_UNIT_MAX_POWER_W_DEFAULT                   240
+
+
+// Power Suply default-Capability Power (W)
+#define LAN9668_POE_UNIT_DEF_POWER_W_DEFAULT                   240
+
+
+// set the PoE MCU controller I2C address (0x1 - 0xFE)
+#define LAN9668_POE_I2C_ADDRESS                                0x20
+
+
+// poe gpio reset pin number
+// 0xFF   - no gpio use for reset poe ports.
+// 0-0xFE - GPIO number used for reset poe ports.
+#define LAN9668_RESET_POE_GPIO_NUMBER                          0xFF
+
+
+//----------------- port map Customizations starts here ------------------//
+
+// BT capability
+#define PD69200BT_CAP  MEBA_POE_PORT_CAP_POE    | \
+                       MEBA_POE_PORT_CAP_TYPE_1 | \
+                       MEBA_POE_PORT_CAP_TYPE_2 | \
+                       MEBA_POE_PORT_CAP_TYPE_3 | \
+                       MEBA_POE_PORT_CAP_TYPE_4 | \
+                       MEBA_POE_PORT_CAP_4PAIR  | \
+                       MEBA_POE_PORT_CAP_FORCE_ON
+
+// AT capability
+#define PD69200AT_CAP   MEBA_POE_PORT_CAP_POE    | \
+                        MEBA_POE_PORT_CAP_TYPE_1 | \
+                        MEBA_POE_PORT_CAP_TYPE_2 | \
+                        MEBA_POE_PORT_CAP_TYPE_3 | \
+                        MEBA_POE_PORT_CAP_FORCE_ON
+
+#define PD69200_CAP ((LAN9668_POE_FIRMWARE_TYPE_DEFAULT == MEBA_POE_FIRMWARE_TYPE_BT) ? PD69200BT_CAP : PD69200AT_CAP)
+
+// pd69200 - AT mapping - 8 PoE ports 2-pairs
+meba_poe_port_properties_t lan9668_pd69200BT_port_map[] =
+{
+//  PoE           | Max   | App-Log | PoE-Log | PoE Pair-A   | PoE Pair-B
+//  Capability    | PWR   | Port-ID | Port-ID | Phys Port-ID | Phys Port-ID
+//---------------------------------------------------------------------------
+  { PD69200BT_CAP , 24000 , 0       , 7       , 4            , 12         },
+  { PD69200BT_CAP , 24000 , 1       , 6       , 2            , 10         },
+  { PD69200BT_CAP , 24000 , 2       , 5       , 6            , 15         },
+  { PD69200BT_CAP , 24000 , 3       , 4       , 0            , 9          },
+  { PD69200BT_CAP , 24000 , 4       , 3       , 7            , 14         },
+  { PD69200BT_CAP , 24000 , 5       , 2       , 1            , 8          },
+  { PD69200BT_CAP , 24000 , 6       , 1       , 5            , 13         },
+  { PD69200BT_CAP , 24000 , 7       , 0       , 3            , 11         }
+};
+
+//------------------- cap and port map Customizations ends here --------------------//
+
+
+//--------------------------------------------------------------------------------------//
+//-------------------------    BT parameters    ----------------------------------------//
+//--------------------------------------------------------------------------------------//
+
+
+// BT System has 4 modes = 15/30/60/90 (applicable for all poe ports)
+// MEBA_POE_PORT_MAX_POWER_15W
+// MEBA_POE_PORT_MAX_POWER_30W
+// MEBA_POE_PORT_MAX_POWER_60W
+// MEBA_POE_PORT_MAX_POWER_90W
+#define LAN9668_BT_PORT_MAX_POWER_DEFAULT                     MEBA_POE_PORT_MAX_POWER_60W
+
+
+// Port Operation Mode for legacy
+// This parameter sets a combination of various port behaviors: Legacy detection, port power, classification fingers, and 4-pair operation.
+// see Microchip PD692x0 BT Serial Communication Protocol Table 3-5. 4-Pair/2-Pair Non-Compliant Modes
+
+#define LAN9668_BT_OPERATION_MODE_LEGACY_90W_POH_DEFAULT              0x25  // Lagacy + PoH 45/90W + BT. No demotion in class 4 or 4,4
+#define LAN9668_BT_OPERATION_MODE_LEGACY_60W_IGNORE_PD_CLASS_DEFAULT  0x21  // Lagacy + IGNORE_PD_CLASS 60W
+#define LAN9668_BT_OPERATION_MODE_LEGACY_90W_IGNORE_PD_CLASS_DEFAULT  0x26  // Lagacy + IGNORE_PD_CLASS 90W
+
+//-- BT individual masks --//
+
+// '0' The allocation logic before classification sums the delivering power ports with
+//     lower priority and adds the result to the available power. After classification, the
+//     disconnection function executes and may disconnect lower priority ports to free
+//     power for the higher priority port.
+// '1' If power is not available for powering-up any port, any new connected port
+//     power-up is denied, regardless of its priority.
+#define LAN9668_INDV_MASK_BT_IGNORE_HIGHER_PRIORITY_DEFAULT     1
+
+
+// '0' Resistor detection range at normal range, according to the IEEE 802.3bt.
+// '1' Open the upper range to 55 K? at 2-pair logical port only.
+//     Notes:
+//     - This feature will not operate on any 4-pair logical port.
+//     - When this mask is set, the capability of PSE Connected to PSE Protection function is reduced.
+#define LAN9668_INDV_MASK_BT_SUPPORT_HIGH_RES_DETECTION_DEFAULT 0
+
+
+// '0' Do not initialize the I2C module in case of inactivity.
+// '1' Initializes the I2C module system after 10 seconds of inactivity.
+#define LAN9668_INDV_MASK_BT_I2C_RESTART_ENABLE_DEFAULT         1
+
+
+// 0 LED stream is disabled.
+// 1 LED stream supports unicolor BT.
+// 2 LED stream supports Bicolor BT.
+// 3 For Microchip internal use only (NTGR).
+// 4 For Microchip internal use only (Direct).
+// 5 Reserved for future:
+//   Direct led function (for 1 port Midspan products).
+//   Vmain LED is driven from the PD692x0 (xSYS_OK, pin 32), and Port LED
+//   is driven from the PD69208 LED port (PD69200 BT LED definitions internal document).
+// 6 Direct led function from PD69208 LED ports (for 1 port Midspan products).
+//   3 LEDs in the same package.
+// 7 Direct led function from PD69208 LED ports (for 1 port Midspan products).
+//   1 package for 2P and 4P LED operation.
+//   Another package for SYS OK pin.
+#define LAN9668_INDV_MASK_BT_LED_STREAM_TYPE_DEFAULT            1
+
+
+// HOCPP - High Over Current Pulse Protection
+// 0 = Internal port startup check duration is 500 ms and HOCPP is enabled immediately (0 ms) after port power-up.
+// 1 = Internal port startup check duration is 500 ms and HOCPP is enabled at the end of this time duration.
+// 2 = Internal port startup check duration is 1000 ms and HOCPP is enabled at the end of this time duration.
+// 3 = Internal port startup check duration is 1500 ms and HOCPP is enabled at the end of this time duration.
+// 4 = Internal port startup check duration is 2000 ms and HOCPP is enabled at the end of this time duration.
+#define LAN9668_INDV_MASK_BT_HOCPP_DEFAULT                      2
+
+
+//PSE powering PSE checking
+// 0 = PSE powering PSE condition does not deny powering new valid ports.
+// 1 = In case PSE powering PSE condition occurs, no additional ports are poweredup, until this problem is resolved.
+#define LAN9668_INDV_MASK_BT_PSE_POWERING_PSE_CHECKING_DEFAULT          1
+
+
+// Layer2 Power Allocation Limit
+// 0 = Power allocation limit up to requested class (non-BT compliant).
+// 1 = Power allocation limit up to minimum between the requested class and the operation mode (BT compliant).
+#define LAN9668_INDV_MASK_BT_LAYER2_POWER_ALLOCATION_LIMIT_DEFAULT      1
+
+
+// Port LED Blinks at invalid signature or connection-check error
+// 0 = When port detects invalid signature or connection-check error, LED stays off.
+// 1 = When port detects invalid signature or connection-check error, LED blinks.
+#define LAN9668_INDV_MASK_BT_PORT_LED_BLINKS_AT_INVALID_SIGNATURE_OR_CONNECTION_CHECK_ERROR_DEFAULT 1
+
+
+// Support_adding lldp_half_priority
+// 0 = Port at LLDP does not have additional half priority.
+// 1 = Port at LLDP has additional half priority compared to non LLDP port at the same priority settings.
+#define LAN9668_INDV_MASK_BT_SUPPORT_ADDING_LLDP_HALF_PRIORITY_DEFAULT   1
+
+
+//--------------------------------------------------------------------------------------//
+//-------------------------    AT parameters    ----------------------------------------//
+//--------------------------------------------------------------------------------------//
+
+
+// '0' If higher priority port powers up and its power exceeds power limit,
+//     a lowest priority port will be disconnected instead.
+// '1' If power is not available for powering up any port,
+//     any new connected port power up will be denied, regardless of its priority.
+#define LAN9668_INDV_MASK_AT_IGNORE_HIGHER_PRIORITY_DEFAULT     0
+
+
+// '0' Don?t support legacy detection.
+// '1' Support legacy detection.
+#define LAN9668_INDV_MASK_AT_SUPPORTS_LEGACY_DETECTION_DEFAULT  0
+
+
+// '0' Disables notification.
+// '1' MESSAGE_READY pin, can be used to notify the host that a reply message is ready.
+//     Refer to PD69200 datasheet or PD69200M shared memory documentation.
+#define LAN9668_INDV_MASK_AT_MESSAGE_READY_NOTIFY_DEFAULT       0
+
+
+// '0' Layer 2 PD commands will be Ignored and Layer 2 PSE requests will return with zero allocation.
+// '1' Layer 2 operation is enabled. Layer 2 commands are processed.
+#define LAN9668_INDV_MASK_AT_LAYER2_LLDP_ENABLE_DEFAULT         1
+
+
+// '0' Priority information received from LLDP/CDP PD message is ignored. (Mask 0x2E must be '1').
+// '1' Port Priority can be defined by PD. (Mask 0x2E must be '1').
+#define LAN9668_INDV_MASK_AT_LAYER2_PRIORITY_BY_PD_DEFAULT      1
+
+
+// '0' Uses old matrix command (2-pair).
+// '1' Uses new 4-pair matrix commands.
+#define LAN9668_INDV_MASK_AT_MATRIX_SUPPORT_4P_DEFAULT          1
+
+
+// -----------  Legacy Power Management mode of operation  ----------------------//
+
+// PM1: Selects the method of calculating total power consumption.
+// PM2: Selects the power limit at the port (maximum or according to class or predefined).
+// PM3: Selects the start condition. (Not recommended for new designs, keep 0x00).
+
+// PM-1 How to calculate system power
+//  0x00 - Full Dynamics (Consumption).
+//  0x01 - LLDP & Classes 1 to 3 = Static power (TPPL), Classes 0,4 = Dynamic.
+//  0x02 - LLDP & Classes 0 to 3 = Static power (TPPL), Class 4 = Dynamic.
+//  0x03 - LLDP & Classes 1 to 4 Static power (TPPL), Class 0 = Dynamic.
+//  0x04 - LLDP & Classes 0 to 4 = Static power (TPPL).
+//  0x05 - LLDP = Static power (TPPL), Non LLDP = Dynamic.
+//  0x06 - LLDP & classes 4 = Static power (TPPL), Classes 0 to 3 = Dynamic.
+//  0x80 - User defined per port (See 4.3.12, Sum_as_TPPL field).
+#define LAN9668_PM1_AT_DEFAULT             0x05
+
+
+//PM-2 Port Power Limit
+//  0 - Table set by the user (PPL)
+//  1 - Class power Limit ? (*)
+//      Port Behavior Equal AF: Class 1 power = 5w or 4w
+//          Class 2 power = 8w or 7w
+//          Class 0,3,4 power = 16.4w or 15.4w Port Behavior Equal AT:
+//          Class 0 to 4 power = 33w or 32w or 30w
+//      Port Behavior Equal POH:
+//          Class 0 to 4 power = 48.7w
+// Note: 1. In 4-pair delivering port, the above power values are doubled.
+//       2. The power values are pre-defined as part of the release and may change between releases.
+//  2 - ICUT Max (According to port behavior) ? (*)
+//      AF - 375mA
+//      AT ? 644mA
+//      POH ? 995mA
+// Note: (*). In 4-pair delivering port, the above power values are doubled.
+//  3 - PPL_Class_Max (The maximum value between PPL and Class).
+//  0x80 - User defined per port (See 4.3.12, PortPM2 nibble field).
+#define LAN9668_PM2_AT_DEFAULT             0x01
+
+
+//PM-3 Start up conditions: The port will not start up in case detected class power is higher than PPL Value
+//  0 - No Condition   Capability
+//  1 - Condition on Classes 1 to 3
+//  2 - Condition on Classes 0 to 3
+//  3 - Condition on Classes 1 to 4
+//  4 - Condition on Classes 0 to 4
+//  0x80 ? User defined per port (See 4.3.12, PortPM3 nibble field)
+//Note: 1. Class power for startup condition is according to the class power
+//         parameters in the release_DB regardless of other masks settings.
+//      2. Other values for this field will be ignored, maintaining the last configuration.
+#define LAN9668_PM3_AT_DEFAULT             0x00
+
+#endif // _LAN9668_POE_HW_CFG_H_
---
+++ meba/src/lan966x/meba_poe.c
@@ -0,0 +1,229 @@
+// Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
+// SPDX-License-Identifier: MIT
+
+#include "microchip/ethernet/board/api.h"
+#include "meba_poe_generic.h"
+#include "lan966x_poe_hw_cfg.h"
+
+
+meba_poe_parameters_t tPoE_parameters =
+{
+    .ePoE_Controller_Type_default = LAN9668_POE_PD692x0_CONTROLLER_TYPE_DEFAULT, // PD692x0 family detection method
+    .ePoE_port_max_power_default  = LAN9668_BT_PORT_MAX_POWER_DEFAULT,           // System has 4 modes = 15/30/60/90 (applicable for all poe ports)
+
+    // Port Operation Mode for legacy
+    .bt_operation_mode_legacy_90W_poh_default             = LAN9668_BT_OPERATION_MODE_LEGACY_90W_POH_DEFAULT,
+    .bt_operation_mode_legacy_60W_ignore_pd_class_default = LAN9668_BT_OPERATION_MODE_LEGACY_60W_IGNORE_PD_CLASS_DEFAULT,
+    .bt_operation_mode_legacy_90W_ignore_pd_class_default = LAN9668_BT_OPERATION_MODE_LEGACY_90W_IGNORE_PD_CLASS_DEFAULT,
+
+    .reset_poe_gpio_number                           = LAN9668_RESET_POE_GPIO_NUMBER,
+
+    .indv_mask_AT_ignore_higher_priority_default     = LAN9668_INDV_MASK_AT_IGNORE_HIGHER_PRIORITY_DEFAULT,     // power higher priority port.
+    .indv_mask_AT_supports_legact_detection_default  = LAN9668_INDV_MASK_AT_SUPPORTS_LEGACY_DETECTION_DEFAULT,  // En/Dis support of legacy detection.
+    .indv_mask_AT_message_ready_notify_default       = LAN9668_INDV_MASK_AT_MESSAGE_READY_NOTIFY_DEFAULT,       // en/Dis MESSAGE_READY pin notification.
+    .indv_mask_AT_layer2_lldp_enable_default         = LAN9668_INDV_MASK_AT_LAYER2_LLDP_ENABLE_DEFAULT,         // En/Dis Layer 2 PD commands.
+    .indv_mask_AT_layer2_priority_by_PD_default      = LAN9668_INDV_MASK_AT_LAYER2_PRIORITY_BY_PD_DEFAULT,      // accept/ignored Port Priority recived from the PD
+    .indv_mask_AT_matrix_support_4P_default          = LAN9668_INDV_MASK_AT_MATRIX_SUPPORT_4P_DEFAULT,          // use 4-pair matrix commands.
+    .indv_mask_BT_ignore_higher_priority_default     = LAN9668_INDV_MASK_BT_IGNORE_HIGHER_PRIORITY_DEFAULT,     // power higher priority port.
+    .indv_mask_BT_support_high_res_detection_default = LAN9668_INDV_MASK_BT_SUPPORT_HIGH_RES_DETECTION_DEFAULT, // expand Resistor detection range up to range to 55 K.
+    .indv_mask_BT_i2c_restart_enable_default         = LAN9668_INDV_MASK_BT_I2C_RESTART_ENABLE_DEFAULT,         // Initialization of the I2C module system after 10 seconds of inactivity.
+    .indv_mask_BT_led_stream_type_default            = LAN9668_INDV_MASK_BT_LED_STREAM_TYPE_DEFAULT,            // led stream type
+    .indv_mask_BT_HOCPP_default                         = LAN9668_INDV_MASK_BT_HOCPP_DEFAULT,                      // HOCPP - high_over Current Pulse Protection
+    .indv_mask_BT_PSE_powering_PSE_checking_default     = LAN9668_INDV_MASK_BT_PSE_POWERING_PSE_CHECKING_DEFAULT,               // PSE powering PSE checking
+    .indv_mask_BT_layer2_power_allocation_limit_default = LAN9668_INDV_MASK_BT_LAYER2_POWER_ALLOCATION_LIMIT_DEFAULT,           // Layer2 Power Allocation Limit
+    .indv_mask_BT_Port_LED_blinks_at_invalid_signature_or_connection_check_error_default = LAN9668_INDV_MASK_BT_PORT_LED_BLINKS_AT_INVALID_SIGNATURE_OR_CONNECTION_CHECK_ERROR_DEFAULT,  // Port LED Blinks at invalid signature or connection-check error
+    .indv_mask_BT_support_adding_lldp_half_priority_default = LAN9668_INDV_MASK_BT_SUPPORT_ADDING_LLDP_HALF_PRIORITY_DEFAULT,   // Support adding lldp half priority
+
+    // -----------  AT Power Management mode of operation  ----------------------//
+    .AT_pm1_default = LAN9668_PM1_AT_DEFAULT,     // Selects the method of calculating total power consumption.
+    .AT_PM2_default = LAN9668_PM2_AT_DEFAULT,     // Selects the power limit at the port (maximum or according to class or predefined).
+    .AT_PM3_default = LAN9668_PM3_AT_DEFAULT      // Selects the start condition. (Not recommended for new designs, keep 0x00).
+};
+
+
+meba_poe_psu_input_prob_t lan9668_power_supplies[] =
+{
+    {
+        .id = MEBA_POE_CTRL_PSU_ALL,                    // PowerSupply-ID
+        .min_w = 0,                                     // PwrSuply Min-Pwr
+        .max_w = LAN9668_POE_UNIT_MAX_POWER_W_DEFAULT,  // PwrSuply Max-Pwr
+        .def_w = LAN9668_POE_UNIT_DEF_POWER_W_DEFAULT,  // PwrSuply Def-Capab
+        .system_consumed_w = 10,                        // System PwrUsage
+        .user_configurable = LAN9668_POE_UNIT_MAX_POWER_USER_CONFIG_DEFAULT     // User Conig 1=Yes,0=No
+    }
+};
+
+meba_poe_system_t lan9668_pd69200_system;
+
+static i2c_config_t lan9668_i2c_config = { "/dev/i2c-1", LAN9668_POE_I2C_ADDRESS };
+
+mesa_rc meba_poe_system_get(
+        meba_inst_t                     inst,
+        meba_poe_system_t             **const system)
+{
+    *system = &lan9668_pd69200_system;
+    return MESA_RC_OK;
+}
+
+
+mesa_rc meba_poe_system_initialize(
+    meba_inst_t            inst,
+    meba_poe_init_params_t *tPoe_init_params)
+{
+    inst->iface.debug(MEBA_TRACE_LVL_NOISE, __FUNCTION__, __LINE__, "Called");
+
+    uint8_t poe_12c0 = lan9668_i2c_config.i2c_address;
+    if (inst->poe_i2c_tags.poe_12c0 != 0)
+    {
+        poe_12c0 = inst->poe_i2c_tags.poe_12c0;
+        //T_I("%s=%d", "poe_12c0", poe_12c0);
+    }
+
+    // overide tMeba_poe_init_params params if using H file parameters
+    if(tPoe_init_params->use_poe_static_parameters) {
+        tPoe_init_params->PwrSupply_MaxPwr              = LAN9668_POE_UNIT_MAX_POWER_W_DEFAULT;
+        tPoe_init_params->eMEBA_POE_FIRMWARE_TYPE       = LAN9668_POE_FIRMWARE_TYPE_DEFAULT; // AF_AT/BT
+        tPoe_init_params->eMEBA_POE_SOFTWARE_POWER_TYPE = (LAN9668_POE_FIRMWARE_TYPE_DEFAULT == MEBA_POE_FIRMWARE_TYPE_BT) ? MEBA_POE_SOFTWARE_POWER_TYPE_BT : MEBA_POE_SOFTWARE_POWER_TYPE_AT;
+    } else { // overide meba power supply by appl init_params
+        lan9668_power_supplies->def_w = tPoe_init_params->PwrSupply_MaxPwr;
+        lan9668_power_supplies->max_w = tPoe_init_params->PwrSupply_MaxPwr;
+    }
+
+    if(tPoe_init_params->eMEBA_POE_FIRMWARE_TYPE == MEBA_POE_FIRMWARE_TYPE_BT) {
+        // Do poe chip detection and fill
+        /* lan9668_ctrl.api = ....; */
+        /* lan9668_ctrl.private_data = ....; */
+        lan9668_pd69200_system.controller_count = 1;
+        lan9668_pd69200_system.controllers = malloc(sizeof(meba_poe_ctrl_inst_t) * lan9668_pd69200_system.controller_count);
+
+        // overide tMeba_poe_init_params params if using H file parameters
+        if(tPoe_init_params->use_poe_static_parameters) {
+            tPoe_init_params->Max_POE_Ch              = sizeof(lan9668_pd69200BT_port_map)/sizeof(meba_poe_port_properties_t);
+        }
+
+        inst->iface.debug(MEBA_TRACE_LVL_INFO, __FUNCTION__, __LINE__,"using:  Max_POE_Ch=%d ,PwrSupply_MaxPwr=%d ,POE_FIRMWARE_TYPE=%d ,POE_SOFTWARE_TYPE=%d",
+              tPoe_init_params->Max_POE_Ch ,
+              tPoe_init_params->PwrSupply_MaxPwr,
+              tPoe_init_params->eMEBA_POE_FIRMWARE_TYPE,
+              tPoe_init_params->eMEBA_POE_SOFTWARE_POWER_TYPE);
+
+        tPoE_parameters.poe_init_params = *tPoe_init_params;
+
+        meba_pd69200bt_driver_init(&lan9668_pd69200_system.controllers[0],
+                                 "pd69x00",
+                                 meba_pd69200_i2c_adapter_open(lan9668_i2c_config.i2c_device, poe_12c0),
+                                 MEBA_POE_CTRL_CAP_POWER_MANAGEMENT  |
+                                 MEBA_POE_CTRL_INTERRUPTIBLE_POWER   |
+                                 MEBA_POE_CTRL_PD_AUTO_CLASS_REQUEST |
+                                 MEBA_POE_CTRL_LEGACY_PD_CLASS_MODE,
+                                 lan9668_pd69200BT_port_map,
+                                 sizeof(lan9668_pd69200BT_port_map)/sizeof(meba_poe_port_properties_t),
+                                 lan9668_power_supplies,
+                                 sizeof(lan9668_power_supplies)/sizeof(meba_poe_psu_input_prob_t),
+                                 inst->iface.debug,
+                                 tPoE_parameters);
+    } else if(tPoe_init_params->eMEBA_POE_FIRMWARE_TYPE == MEBA_POE_FIRMWARE_TYPE_AT) {
+        // Do poe chip detection and fill
+        /* lan9668_ctrl.api = ....; */
+        /* lan9668_ctrl.private_data = ....; */
+        lan9668_pd69200_system.controller_count = 1;
+        lan9668_pd69200_system.controllers = malloc(sizeof(meba_poe_ctrl_inst_t) * lan9668_pd69200_system.controller_count);
+
+        // overide tMeba_poe_init_params params if using H file parameters
+        if(tPoe_init_params->use_poe_static_parameters) {
+            tPoe_init_params->Max_POE_Ch              = sizeof(lan9668_pd69200BT_port_map)/sizeof(meba_poe_port_properties_t);
+        }
+
+        inst->iface.debug(MEBA_TRACE_LVL_INFO, __FUNCTION__, __LINE__,"using:  Max_POE_Ch=%d ,PwrSupply_MaxPwr=%d ,POE_FIRMWARE_TYPE=%d ,POE_SOFTWARE_TYPE=%d",
+              tPoe_init_params->Max_POE_Ch ,
+              tPoe_init_params->PwrSupply_MaxPwr,
+              tPoe_init_params->eMEBA_POE_FIRMWARE_TYPE,
+              tPoe_init_params->eMEBA_POE_SOFTWARE_POWER_TYPE);
+
+        tPoE_parameters.poe_init_params = *tPoe_init_params;
+
+        meba_pd69200_driver_init(&lan9668_pd69200_system.controllers[0],
+                                 "pd69x00",
+                                 meba_pd69200_i2c_adapter_open(lan9668_i2c_config.i2c_device, poe_12c0),
+                                 MEBA_POE_CTRL_CAP_POWER_MANAGEMENT |
+                                 MEBA_POE_CTRL_CAP_PD_LEGACY_DETECTION,
+                                 lan9668_pd69200BT_port_map,
+                                 sizeof(lan9668_pd69200BT_port_map)/sizeof(meba_poe_port_properties_t),
+                                 lan9668_power_supplies,
+                                 sizeof(lan9668_power_supplies)/sizeof(meba_poe_psu_input_prob_t),
+                                 inst->iface.debug,
+                                 tPoE_parameters);
+    }
+    else
+    {
+        return MESA_RC_ERROR;
+    }
+
+    return MESA_RC_OK;
+};
+
+mesa_rc meba_poe_get_controller_handle(meba_inst_t inst,
+                                       mesa_port_no_t port_no,
+                                       meba_poe_ctrl_inst_t **controller,
+                                       meba_poe_port_handle_t *handle)
+{
+    for (int i = 0; i < lan9668_pd69200_system.controller_count; ++i) {
+        for (int j = 0; j<lan9668_pd69200_system.controllers[i].port_map_length; ++j) {
+            if (lan9668_pd69200_system.controllers[i].port_map[j].port_no == port_no) {
+                *controller = &lan9668_pd69200_system.controllers[i];
+                *handle = lan9668_pd69200_system.controllers[i].port_map[j].handle;
+                return MESA_RC_OK;
+            }
+        }
+    }
+    return MESA_RC_ERROR;
+}
+
+// returns values:
+// - MESA_RC_OK : when all controllers are responded (not MESA_RC_ERROR)
+// - MESA_RC_ERROR : when one or more controllers are responded with MESA_RC_ERROR
+mesa_rc meba_poe_do_detection(meba_inst_t inst)
+{
+    mesa_rc rc = MESA_RC_ERROR;
+    inst->iface.debug(MEBA_TRACE_LVL_NOISE, __FUNCTION__, __LINE__, "Called");
+    for (int i = 0; i < lan9668_pd69200_system.controller_count; ++i)
+    {
+        if (lan9668_pd69200_system.controllers[i].api->meba_poe_ctrl_do_detection(&lan9668_pd69200_system.controllers[i]) == MESA_RC_ERROR)
+        {
+            inst->iface.debug(MEBA_TRACE_LVL_INFO, __FUNCTION__, __LINE__, "Detection Failed for controller: %d", i);
+        } else {
+            rc = MESA_RC_OK;
+        }
+    }
+    return rc;
+}
+
+static meba_api_poe_t public_functions = {
+    .meba_poe_system_initialize = meba_poe_system_initialize,
+    .meba_poe_system_get = meba_poe_system_get,
+    .meba_poe_get_controller_handle = meba_poe_get_controller_handle,
+    .meba_poe_do_detection = meba_poe_do_detection,
+    .meba_poe_chip_initialization = meba_poe_generic_chip_initialization,
+    .meba_poe_sync = meba_poe_generic_sync,
+    .meba_poe_version_get = meba_poe_generic_version_get,
+    .meba_poe_port_cfg_set = meba_poe_generic_port_cfg_set,
+    .meba_poe_port_status_get = meba_poe_generic_port_status_get,
+    .meba_poe_cfg_set = meba_poe_generic_cfg_set,
+    .meba_poe_status_get = meba_poe_generic_status_get,
+    .meba_poe_capabilities_get = meba_poe_generic_capabilities_get,
+    .meba_poe_reset_command = meba_poe_generic_reset_command,
+    .meba_poe_save_command = meba_poe_generic_save_command,
+    .meba_poe_debug = meba_poe_generic_debug,
+    .meba_poe_firmware_upgrade = meba_poe_generic_firmware_upgrade,
+    .meba_poe_prepare_firmware_upgrade = meba_poe_generic_prepare_firmware_upgrade,
+    .meba_poe_supply_limits_get = meba_poe_generic_supply_limits_get,
+    .meba_poe_port_capabilities_get = meba_poe_generic_port_capabilities_get,
+    .meba_poe_port_pd_data_set = meba_poe_generic_port_pd_data_set,
+    .meba_poe_port_pd_bt_data_set = meba_poe_generic_port_pd_bt_data_set,
+    .meba_poe_port_pd_data_clear = meba_poe_generic_port_pd_data_clear,
+};
+
+meba_api_poe_t *meba_poe_get()
+{
+    return &public_functions;
+}
