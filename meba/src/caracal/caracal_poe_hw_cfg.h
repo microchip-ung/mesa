b10390ba96c7a2266dddc3833e2d74773e441ca2
ezra <ekoper@microsemi.com>
2023-08-01 08:55:00 +0300

    poe-driver: set individual masks to default values
    Review-record: https://bitbucket.microchip.com/projects/UNGE/repos/sw-mesa/pull-requests/346/

--- meba/src/caracal/caracal_poe_hw_cfg.h
+++ meba/src/caracal/caracal_poe_hw_cfg.h
@@ -79,13 +79,15 @@
 #define BT_OPERATION_MODE_LEGACY_90W_IGNORE_PD_CLASS_DEFAULT  0x26  // Lagacy + IGNORE_PD_CLASS 90W
 
 
+//-- BT individual masks --//
+
 // '0' The allocation logic before classification sums the delivering power ports with
 //     lower priority and adds the result to the available power. After classification, the
 //     disconnection function executes and may disconnect lower priority ports to free
 //     power for the higher priority port.
 // '1' If power is not available for powering-up any port, any new connected port
 //     power-up is denied, regardless of its priority.
-#define INDV_MASK_BT_IGNORE_HIGHER_PRIORITY_DEFAULT     0
+#define INDV_MASK_BT_IGNORE_HIGHER_PRIORITY_DEFAULT     1
 
 
 // '0' Resistor detection range at normal range, according to the IEEE 802.3bt.
@@ -93,7 +95,7 @@
 //     Notes:
 //     - This feature will not operate on any 4-pair logical port.
 //     - When this mask is set, the capability of PSE Connected to PSE Protection function is reduced.
-#define INDV_MASK_BT_SUPPORT_HIGH_RES_DETECTION_DEFAULT 1
+#define INDV_MASK_BT_SUPPORT_HIGH_RES_DETECTION_DEFAULT 0
 
 
 // '0' Do not initialize the I2C module in case of inactivity.
@@ -115,7 +117,7 @@
 // 7 Direct led function from PD69208 LED ports (for 1 port Midspan products).
 //   1 package for 2P and 4P LED operation.
 //   Another package for SYS OK pin.
-#define INDV_MASK_BT_LED_STREAM_TYPE_DEFAULT            2
+#define INDV_MASK_BT_LED_STREAM_TYPE_DEFAULT            0
 
 
 // HOCPP - High Over Current Pulse Protection
@@ -124,7 +126,7 @@
 // 2 = Internal port startup check duration is 1000 ms and HOCPP is enabled at the end of this time duration.
 // 3 = Internal port startup check duration is 1500 ms and HOCPP is enabled at the end of this time duration.
 // 4 = Internal port startup check duration is 2000 ms and HOCPP is enabled at the end of this time duration.
-#define INDV_MASK_BT_HOCPP_DEFAULT                 2
+#define INDV_MASK_BT_HOCPP_DEFAULT                      2
 
 
 //PSE powering PSE checking
@@ -142,7 +144,7 @@
 // Port LED Blinks at invalid signature or connection-check error
 // 0 = When port detects invalid signature or connection-check error, LED stays off.
 // 1 = When port detects invalid signature or connection-check error, LED blinks.
-#define INDV_MASK_BT_PORT_LED_BLINKS_AT_INVALID_SIGNATURE_OR_CONNECTION_CHECK_ERROR_DEFAULT 0
+#define INDV_MASK_BT_PORT_LED_BLINKS_AT_INVALID_SIGNATURE_OR_CONNECTION_CHECK_ERROR_DEFAULT 1
 
 
 // Support_adding lldp_half_priority
