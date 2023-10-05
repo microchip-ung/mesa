103edf0740112c4ed27c02f98c09492f9e571454
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2023-10-05 13:12:59 +0200

    Laguna: Changed default SGPIO clock freq to 5Mhz

--- mesa/demo/port.c
+++ mesa/demo/port.c
@@ -1875,14 +1875,12 @@ static void check_sfp_drv_status(meba_inst_t inst, mesa_port_no_t port_no, mesa_
         entry->sfp_status.los = TRUE;
         return;
     }
-    // To be changed when i2c read issue is resloved
-    meba_sfp_device_info_get(inst, port_no, &info);
     // Read SFP ROM
-    /* if (!meba_sfp_device_info_get(inst, port_no, &info)) { */
-    /*     T_E("Port:%u SFP read (i2c) failed", port_no); */
-    /*     entry->sfp_device = NULL; */
-    /*     return; */
-    /* } */
+    if (!meba_sfp_device_info_get(inst, port_no, &info)) {
+        T_E("Port:%u SFP read (i2c) failed", port_no);
+        entry->sfp_device = NULL;
+        return;
+    }
 
     // Search for pre-installed drivers (based on product name)
     // If not found then install the driver according to the MSA standard
