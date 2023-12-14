beed582a1d650155de9339d711aa32e9fb52869f
Palle Christensen <palle.christensen@microchip.com>
2023-12-14 13:32:45 +0100

    Laguna: Fix one sec wrap around issue related to phy time stamping

--- base/fa/vtss_fa_packet.c
+++ base/fa/vtss_fa_packet.c
@@ -132,6 +132,7 @@ static vtss_rc fa_packet_ns_to_ts_cnt(vtss_state_t  *vtss_state,
     _vtss_ts_domain_timeofday_get(NULL, 0, &ts, &tc);
     if (ts.nanoseconds < frame_ns) {
         tod_ns = ts.nanoseconds + VTSS_ONE_MIA; /* TOD nanoseconds is smaller than the frame_ns from the frame. TOD nanoseconds has wrapped */
+        tc += ((u64)VTSS_ONE_MIA) << 16;
     } else {
         tod_ns = ts.nanoseconds;
     }
