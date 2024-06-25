35303b66d949c2658406da68123cf4ea0b593b52
Claus Jensen <claus.jensen@microchip.com>
2024-06-25 09:37:06 +0000

    CPU mirror tests added

--- mesa/demo/test/l2_mirror.rb
+++ mesa/demo/test/l2_mirror.rb
@@ -7,106 +7,274 @@ require_relative 'libeasy/et'
 
 $ts = get_test_setup("mesa_pc_b2b_4x")
 
-#---------- Configuration -----------------------------------------------------
+test_table = [
+    {
+        txt: "mirror-ingress-port",
+        mir: {idx: 3},
+        igr: {idx: 0},
+        frm: [
+            # Learn frame
+            {tx: {idx: 1, eth: "smac 11"}, rx: {idx: [0,2]}},
+            # Ingress mirror
+            {tx: {idx: 0, eth: "dmac 11 smac 10"}, rx: {idx: [1,3]}}
+        ]
+    },
+    {
+        txt: "mirror-egress-port",
+        mir: {idx: 0},
+        egr: {idx: 3},
+        frm: [
+            # Learn frame
+            {tx: {idx: 3, eth: "smac 13"}, rx: {idx: [1,2]}},
+            # Egress mirror
+            {tx: {idx: 2, eth: "dmac 13 smac 12"}, rx: {idx: [0,3]}}
+        ]
+    },
+    {
+        txt: "mirror-ingress-vlan",
+        mir: {idx: 1},
+        vln: {vid: 1, mirror: true, idx: []},
+        frm: [
+            # VLAN mirror
+            {tx: {idx: 3}, rx: {idx: [1]}}
+        ]
+    },
+    {
+        txt: "mirror-acl",
+        mir: {idx: 3},
+        ace: {id: 1, idx: 0, type: "MESA_ACE_TYPE_IPV6"},
+        frm: [
+            # Learn frame
+            {tx: {idx: 1, eth: "smac 11"}, rx: {idx: [0,2]}},
+            # ACL
+            {tx: {idx: 0, eth: "dmac 11 ipv6"}, rx: {idx: [1,3]}}
+        ]
+    },
+    {
+        txt: "mirror-tag",
+        cap: "L2_MIRROR_TAG",
+        mir: {idx: 3, tag: "MESA_MIRROR_TAG_S", vid: 10, pcp: 7},
+        igr: {idx: 0},
+        vln: {vid: 1, mirror: false, idx: []},
+        frm: [
+            # Ingress mirror
+            {tx: {idx: 0, eth: "data pattern cnt 64"},
+             rx: {idx: [3], eth: nil, tag: {tpid: 0x88a8, vid: 10, pcp: 7}}}
+        ]
+    },
+    {
+        txt: "mirror-cpu-ingress",
+        mir: {idx: 3},
+        cpu: {igr: true},
+        npi: {idx: 0},
+        frm: [
+            # CPU ingress mirror
+            {tx: {idx: 0, ifh: {idx: 2}}, rx: {idx: [2,3]}}
+        ]
+    },
+    {
+        txt: "mirror-cpu-egress",
+        mir: {idx: 3},
+        cpu: {egr: true},
+        frm: [
+            # CPU egress mirror
+            {tx: {idx: 0, eth: "dmac 01:80:c2:00:00:00"}, rx: {idx: [3]}}
+        ]
+    },
+=begin
+    {
+        txt: "mirror-egress-cpu-tx",
+        mir: {idx: 0},
+        egr: {idx: 1},
+        npi: {idx: 2},
+        frm: [
+            # Egress mirror of frame transmitted by CPU
+            {tx: {idx: 2, ifh: {idx: 1}}, rx: {idx: [0,1]}}
+        ]
+    },
+=end
+]
 
-$port_idx_rx     = 0 # Ingress mirroring
-$port_idx_tx     = 1 # Egress mirroring
-$port_idx_normal = 2 # Normal forwarding and VLAN/ACL mirroring
-$port_idx_mirror = 3 # Mirror port
-
-test "conf" do
-    t_i("Normal port is a C-port");
-    port_normal = $ts.dut.port_list[$port_idx_normal]
-    conf = $ts.dut.call("mesa_vlan_port_conf_get", port_normal)
-    conf["port_type"] = "MESA_VLAN_PORT_TYPE_C"
-    $ts.dut.call("mesa_vlan_port_conf_set", port_normal, conf)
-
-    t_i("Mirror port")
-    conf = $ts.dut.call("mesa_mirror_conf_get")
-    conf["port_no"] = $ts.dut.port_list[$port_idx_mirror]
-    conf["tag"] = "MESA_MIRROR_TAG_NONE"
-    $ts.dut.call("mesa_mirror_conf_set", conf)
-
-    t_i("Ingress mirroring")
-    $ts.dut.call("mesa_mirror_ingress_ports_set", port_idx_list_str([$port_idx_rx]))
-
-    t_i("Egress mirroring")
-    $ts.dut.call("mesa_mirror_egress_ports_set", port_idx_list_str([$port_idx_tx]))
-
-    t_i("VLAN mirroring")
-    vid = 100
-    conf = $ts.dut.call("mesa_vlan_vid_conf_get", vid)
-    conf["mirror"] = true
-    conf = $ts.dut.call("mesa_vlan_vid_conf_set", vid, conf)
-
-    t_i("ACL rule mirroring IPv6 frames on normal port")
-    ace = $ts.dut.call("mesa_ace_init", "MESA_ACE_TYPE_IPV6")
-    ace["id"] = 1
-    ace["port_list"] = "#{port_normal}"
-    ace["action"]["mirror"] = true
-    $ts.dut.call("mesa_ace_add", 0, ace)
-end
-
-#---------- Frame testing -----------------------------------------------------
-
-def cmd_base(idx_tx, idx_rx)
-    mac_base = "00:00:00:00:00:0"
-    smac = (mac_base + "#{idx_tx + 1}")
-    dmac = (mac_base + "#{idx_rx + 1}")
-    cmd = "eth dmac #{dmac} smac #{smac}"
-end
-
-def mirror_frame_test(txt, idx_tx, idx_rx, idx_list, cmd_add = "")
-    t_i("#{txt} frame on port index #{idx_tx}")
-    cmd = cmd_base(idx_tx, idx_rx) + cmd_add
-    run_ef_tx_rx_cmd($ts, idx_tx, idx_list, cmd)
-end
-
-test "frame-io" do
-    idx_rx = $port_idx_rx
-    idx_tx = $port_idx_tx
-    idx_normal = $port_idx_normal
-    idx_mirror = $port_idx_mirror
-
-    mirror_frame_test("Learn", idx_rx, idx_mirror, [idx_tx, idx_normal, idx_mirror])
-    mirror_frame_test("Learn", idx_tx, idx_mirror, [idx_rx, idx_normal])
-    mirror_frame_test("Learn", idx_normal, idx_mirror, [idx_rx, idx_tx, idx_mirror])
-    mirror_frame_test("Rx mirror", idx_rx, idx_normal, [idx_normal, idx_mirror])
-    mirror_frame_test("Normal", idx_tx, idx_normal, [idx_normal])
-    mirror_frame_test("Tx mirror", idx_normal, idx_tx, [idx_tx, idx_mirror])
-    mirror_frame_test("Normal", idx_normal, idx_rx, [idx_rx])
-
-    # Add tag and avoid switch padding after popping tag
-    f_end = " data pattern cnt 64"
-    cmd_add = cmd_tag_push({tpid: 0x8100, vid: 100}) + f_end
-    mirror_frame_test("VLAN mirror", idx_normal, idx_rx, [idx_mirror], cmd_add)
-
-    mirror_frame_test("ACL mirror", idx_normal, idx_rx, [idx_rx, idx_mirror], " ipv6")
-
-    if (cap_get("L2_MIRROR_TAG") != 0)
-        # Pushed tag on mirror port
-        tag = {tpid: 0x88a8, vid: 42, pcp: 5, dei: 1}
-
-        t_i("Enable mirror tagging")
-        conf = $ts.dut.call("mesa_mirror_conf_get")
-        conf["tag"] = "MESA_MIRROR_TAG_S"
-        conf["vid"] = tag[:vid]
-        conf["pcp"] = tag[:pcp]
-        conf["dei"] = tag[:dei]
-        $ts.dut.call("mesa_mirror_conf_set", conf)
-
-        # Send the IPv6 frame (f1) again, expect S-tagged frame (f2) on mirror port
-        f1 = cmd_base(idx_normal, idx_rx)
-        f2 = f1
-        f1 += " ipv6"
-        f1 += f_end
-        f2 += cmd_tag_push(tag)
-        f2 += " ipv6"
-        f2 += f_end
-        cmd = "sudo ef name f1 #{f1} name f2 #{f2}"
-        cmd += " tx #{$ts.pc.p[idx_normal]} name f1"
-        cmd += " rx #{$ts.pc.p[idx_rx]} name f1"
-        cmd += " rx #{$ts.pc.p[idx_mirror]} name f2"
-        $ts.pc.run(cmd)
+def cmd_frame(eth, tag)
+    cmd = "eth"
+    if (eth != nil)
+        cmd += " #{eth}"
     end
+    if (tag != nil)
+        tpid = fld_get(tag, :tpid, 0x8100)
+        cmd += cmd_tag_push({tpid: tpid, vid: tag[:vid], pcp: tag[:pcp]})
+        cmd += " data pattern cnt 64"
+    end
+    return cmd
+end
+
+def mirror_test(t)
+    # Capability check
+    cap = t[:cap]
+    if (cap != nil and cap_get(cap) == 0)
+        return
+    end
+
+    # Mirror configuration
+    mir = t[:mir]
+    c = $ts.dut.call("mesa_mirror_conf_get")
+    port_none = c["port_no"]
+    c["port_no"] = $ts.dut.p[mir[:idx]]
+    tag = mir[:tag]
+    if (tag != nil)
+        c["tag"] = tag
+        c["vid"] = mir[:vid]
+        c["pcp"] = mir[:pcp]
+    end
+    $ts.dut.call("mesa_mirror_conf_set", c)
+
+    # NPI port
+    npi = t[:npi]
+    if (npi != nil)
+        c = $ts.dut.call("mesa_npi_conf_get")
+        c["enable"] = true
+        c["port_no"] = $ts.dut.p[npi[:idx]]
+        $ts.dut.call("mesa_npi_conf_set", c)
+    end
+
+    # Ingress mirroring
+    igr = t[:igr]
+    if (igr != nil)
+        $ts.dut.call("mesa_mirror_ingress_ports_set", "#{$ts.dut.p[igr[:idx]]}")
+    end
+
+    # Egress mirroring
+    egr = t[:egr]
+    if (egr != nil)
+        $ts.dut.call("mesa_mirror_egress_ports_set", "#{$ts.dut.p[egr[:idx]]}")
+    end
+
+    # CPU mirroring
+    cpu = t[:cpu]
+    if (cpu != nil)
+        if (cpu[:igr] != nil)
+            $ts.dut.call("mesa_mirror_cpu_ingress_set", true)
+        end
+        if (cpu[:egr] != nil)
+            $ts.dut.call("mesa_mirror_cpu_egress_set", true)
+        end
+    end
+
+    # VLAN mirroring
+    vlan = t[:vln]
+    old = nil
+    if (vlan != nil)
+        vid = vlan[:vid]
+        c = $ts.dut.call("mesa_vlan_vid_conf_get", vid)
+        c["mirror"] = vlan[:mirror]
+        $ts.dut.call("mesa_vlan_vid_conf_set", vid, c)
+        old = $ts.dut.call("mesa_vlan_port_members_get", vid)
+        $ts.dut.call("mesa_vlan_port_members_set", vid, port_idx_list_str(vlan[:idx]))
+    end
+
+    # ACL mirroring
+    ace = t[:ace]
+    if (ace != nil)
+        c = $ts.dut.call("mesa_ace_init", ace[:type])
+        c["id"] = ace[:id]
+        c["port_list"] = "#{$ts.dut.p[ace[:idx]]}"
+        c["action"]["mirror"] = true
+        $ts.dut.call("mesa_ace_add", 0, c)
+    end
+
+    # Frame test
+    frm = fld_get(t, :frm, [])
+    frm.each do |f|
+        cmd = "ef name f_tx "
+        tx = f[:tx]
+        eth = tx[:eth]
+        tag = tx[:tag]
+        ifh = tx[:ifh]
+        if (ifh != nil)
+            cmd += cmd_tx_ifh_push({"dst_port": $ts.dut.p[ifh[:idx]]})
+            eth = "data pattern cnt 64"
+        end
+        cmd += cmd_frame(eth, tag)
+        cmd += " name f_rx "
+        rx = f[:rx]
+        eth = fld_get(rx, :eth, eth)
+        tag = fld_get(rx, :tag, tag)
+        cmd += cmd_frame(eth, tag)
+        [0,1,2,3].each do |idx|
+            if (idx == tx[:idx])
+                cmd += " tx #{$ts.pc.p[idx]} name f_tx"
+            else
+                cmd += " rx #{$ts.pc.p[idx]}"
+                if (rx[:idx].include?idx)
+                    cmd += " name f_rx"
+                end
+            end
+        end
+        $ts.pc.try(cmd)
+    end
+
+    # Return here when debugging a test
+    #return
+
+    # Restore mirror configuration
+    c = $ts.dut.call("mesa_mirror_conf_get")
+    c["port_no"] = port_none
+    c["tag"] = "MESA_MIRROR_TAG_NONE"
+    $ts.dut.call("mesa_mirror_conf_set", c)
+
+    # Restore NPI configuration
+    npi = t[:npi]
+    if (npi != nil)
+        c = $ts.dut.call("mesa_npi_conf_get")
+        c["enable"] = false
+        $ts.dut.call("mesa_npi_conf_set", c)
+    end
+
+    # Restore ingress mirroring
+    if (igr != nil)
+        $ts.dut.call("mesa_mirror_ingress_ports_set", "")
+    end
+
+    # Restore egress mirroring
+    if (egr != nil)
+        $ts.dut.call("mesa_mirror_egress_ports_set", "")
+    end
+
+    # Restore CPU mirroring
+    if (cpu != nil)
+        if (cpu[:igr] != nil)
+            $ts.dut.call("mesa_mirror_cpu_ingress_set", false)
+        end
+        if (cpu[:egr] != nil)
+            $ts.dut.call("mesa_mirror_cpu_egress_set", false)
+        end
+    end
+
+    # Restore VLAN mirroring
+    if (vlan != nil)
+        vid = vlan[:vid]
+        c = $ts.dut.call("mesa_vlan_vid_conf_get", vid)
+        c["mirror"] = false
+        $ts.dut.call("mesa_vlan_vid_conf_set", vid, c)
+        $ts.dut.call("mesa_vlan_port_members_set", vid, old)
+    end
+
+    # Remove ACL rule
+    if (ace != nil)
+        ace = $ts.dut.call("mesa_ace_del", ace[:id])
+    end
+end
+
+sel = table_lookup(test_table, :sel)
+test_table.each do |t|
+    next if (t[:sel] != sel)
+    test t[:txt] do
+        mirror_test(t)
+    end
+end
+test_summary
+
+test "dump" do
+    break
+    $ts.dut.run("mesa-cmd deb api mirror")
 end
