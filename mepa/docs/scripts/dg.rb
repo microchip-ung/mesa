37c1de925193d3bef24b616f41f69ccc0a40c18e
Allan W. Nielsen <allan.nielsen@microchip.com>
2024-06-26 16:29:34 +0200

    dg: Allow running without bundler
    Review-record: https://bitbucket.microchip.com/projects/UNGE/repos/sw-mesa/pull-requests/573

--- mepa/docs/scripts/dg.rb
+++ mepa/docs/scripts/dg.rb
@@ -3,9 +3,6 @@
 # Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
 # SPDX-License-Identifier: MIT
 
-require 'rubygems'
-require 'bundler/setup'
-
 require 'pp'
 require 'yaml'
 require 'json'
