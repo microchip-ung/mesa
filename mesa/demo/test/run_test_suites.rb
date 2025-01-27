03cfef8715ef44719500adf7579b96a496b8eafb
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2025-01-27 16:24:52 +0100

    Corrected typo in last checkin

--- mesa/demo/test/run_test_suites.rb
+++ mesa/demo/test/run_test_suites.rb
@@ -83,7 +83,7 @@ puts "-----Download and unpack all test folders from Jenkins-----"
 $systems.each { |system|
     # Compose path to the image and test folder of the requested branch
 #    jenkins_images = "http://soft00.microsemi.net:8080/job/API-mesa/job/" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
-    jenkins_images = "https://ung.jenkins.microchip.com/job/UNGE/job/sw-mesa/job" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
+    jenkins_images = "https://ung.jenkins.microchip.com/job/UNGE/job/sw-mesa/job/" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
 
     puts("Download latest test folder from jenkins")
     dl_file "#{jenkins_images}/et.tar.gz", "et.tar.gz"
@@ -100,7 +100,7 @@ $sequential_threads = []
 def start_test(system)
     # Compose path to the image and test folder of the requested branch
 #    jenkins_images = "http://soft00.microsemi.net:8080/job/API-mesa/job/" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
-    jenkins_images = "https://ung.jenkins.microchip.com/job/UNGE/job/sw-mesa/job" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
+    jenkins_images = "https://ung.jenkins.microchip.com/job/UNGE/job/sw-mesa/job/" + system[:branch] + "/lastSuccessfulBuild/artifact/images"
     puts("Start test suites on system #{system[:name]} in a thread")
     t = Thread.new do
         system "GIT_BRANCH_NAME=#{system[:branch]} ./utils/run-suites-on.rb --system #{system[:name]} --dir #{system[:name]}-#{system[:branch]}-test/test --image #{jenkins_images}/#{system[:image]}"
