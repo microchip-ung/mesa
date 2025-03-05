869ff9ce30abe5c63bd51e39719fd583cee8c0ac
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2025-03-05 13:03:42 +0100

    ET: If not log option is given then no log is created (default) in run.rb

--- mesa/demo/test/utils/run.rb
+++ mesa/demo/test/utils/run.rb
@@ -11,87 +11,98 @@ require 'optparse'
 
 $options = {}
 $options[:times] = 1
+$options[:out_folder] = nil  # Default: no output folder, suppress logs
+
 OptionParser.new do |opts|
-    opts.banner = "Usage: run.rb [options] -- list of tests"
-    opts.banner += "\nExample: utils/run.rb `find -name \"l3_ip_mc*.rb\"` -t 10 -o log"
-    opts.on("-h", "--help", "This message") do |v|
-        puts opts
-        exit
-    end
+  opts.banner = "Usage: run.rb [options] -- list of tests"
+  opts.banner += "\nExample: utils/run.rb `find -name \"l3_ip_mc*.rb\"` -t 10 -o log"
 
-    opts.on("-t", "--times cnt", "How many times to repeat the test") do |t|
-        $options[:times] = t.to_i
-    end
+  opts.on("-h", "--help", "This message") do
+    puts opts
+    exit
+  end
 
-    $options[:stop] = false
-    opts.on("-s", "--stop", "Stop if test fails") do |s|
-        $options[:stop] = s
-    end
+  opts.on("-t", "--times cnt", "How many times to repeat the test") do |t|
+    $options[:times] = t.to_i
+  end
 
-    opts.on("-o", "--output folder", "Folder to store the output in") do |f|
-        $options[:out_folder] = f
-    end
+  $options[:stop] = false
+  opts.on("-s", "--stop", "Stop if test fails") do
+    $options[:stop] = true
+  end
+
+  opts.on("-o", "--output folder", "Folder to store the output in") do |f|
+    $options[:out_folder] = f
+  end
 end.parse!
 
-if $options[:out_folder].nil?
-    $stderr.puts "Output folder missing"
-    exit -1
+# Convert relative filenames (without path) to ./filename format
+ARGV.map! do |test|
+  if !test.start_with?("/", "./") && File.exist?(test)  # If file exists locally and is not an absolute/relative path
+    "./#{test}"
+  else
+    test  # Keep as-is
+  end
 end
 
-if File.exist? $options[:out_folder]
+# Create output folder if specified
+if $options[:out_folder]
+  if File.exist? $options[:out_folder]
     system("rm -fr #{$options[:out_folder]}")
-end
+  end
 
-if not system("mkdir -p #{$options[:out_folder]}")
+  unless system("mkdir -p #{$options[:out_folder]}")
     $stderr.puts "Could not create output folder"
     exit -1
+  end
 end
 
 $ok = 0
 $err = 0
 
-
 puts "Tests to be executed:"
-ARGV.each { |test|
-    puts "#{test}"
-}
+ARGV.each { |test| puts test }
 
 $options[:times].times do |i|
-    begin
-        o = File.open("#{$options[:out_folder]}/run-%04d.stdout" % [i+1], 'w')
-        e = File.open("#{$options[:out_folder]}/run-%04d.stderr" % [i+1], 'w')
-
-        ARGV.each { |test|
-            t1 = Time.now
-            print "%s %04d/%04d-%04d %-30s" % [Time.now.strftime("%y-%m-%d %T"), i+1, $options[:times], $err, test]
-            pid = spawn("#{test}", {:out => o, :err => e})
-            Process.wait pid
-            res = $?
-            t2 = Time.now
-            s = ""
-            took = "took #{t2 - t1} seconds"
-            if res.exitstatus == 0
-                File.write("#{$options[:out_folder]}/run-%04d.ok" % [i+1], "#{res.exitstatus}")
-                $ok += 1
-                s = "ok"
-            else
-                File.write("#{$options[:out_folder]}/run-%04d.not-ok" % [i+1], "#{res.exitstatus}")
-                $err += 1
-                s = "not-ok - #{res.exitstatus}"
-                if $err > 0 && $options[:stop]
-                    break
-                end
-            end
-            puts "#{s} #{took}"
-        }
-        if $err > 0 && $options[:stop]
-            break
-        end
-
-    ensure
-        o.close if not o.nil?
-        e.close if not e.nil?
+  begin
+    o = e = nil
+    if $options[:out_folder]
+      o = File.open("#{$options[:out_folder]}/run-%04d.stdout" % [i + 1], 'w')
+      e = File.open("#{$options[:out_folder]}/run-%04d.stderr" % [i + 1], 'w')
+    else
+      o = e = File.open("/dev/null", "w")  # Suppress all output
     end
+
+    ARGV.each do |test|
+      t1 = Time.now
+      print "%s %04d/%04d-%04d %-30s" % [Time.now.strftime("%y-%m-%d %T"), i + 1, $options[:times], $err, test]
+
+      pid = spawn("#{test}", out: o, err: e)  # Redirect both stdout and stderr
+
+      Process.wait pid
+      res = $?
+      t2 = Time.now
+      took = "took #{t2 - t1} seconds"
+
+      if res.exitstatus == 0
+        File.write("#{$options[:out_folder]}/run-%04d.ok" % [i + 1], "#{res.exitstatus}") if $options[:out_folder]
+        $ok += 1
+        s = "ok"
+      else
+        File.write("#{$options[:out_folder]}/run-%04d.not-ok" % [i + 1], "#{res.exitstatus}") if $options[:out_folder]
+        $err += 1
+        s = "not-ok - #{res.exitstatus}"
+        break if $err > 0 && $options[:stop]
+      end
+      puts "#{s} #{took}"
+    end
+
+    break if $err > 0 && $options[:stop]
+
+  ensure
+    o&.close
+    e&.close
+  end
 end
 
 puts "#{Time.now.strftime("%y-%m-%d %T")}  END - Error cnt: #{$err}"
