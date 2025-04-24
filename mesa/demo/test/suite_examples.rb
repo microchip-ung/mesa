b6512dd71cee5a8a8638bb522c5f2e0292e7c117
Claus Jensen <claus.jensen@microchip.com>
2025-04-24 09:04:50 +0200

    Added RedBox example to test suite

--- mesa/demo/test/suite_examples.rb
+++ mesa/demo/test/suite_examples.rb
@@ -14,14 +14,15 @@ run %w{./example_l2_evlan.rb}
 run %w{./example_l2_stp.rb}
 run %w{./example_l2_vlan.rb}
 run %w{./example_l3_ip_uc.rb}
+run %w{./example_mrp.rb}
 run %w{./example_packet_rx_tx.rb}
+run %w{./example_port_conf_bulk.rb}
+run %w{./example_port_dynamic.rb}
 run %w{./example_qos-time-aware-scheduler.rb}
+run %w{./example_redbox.rb}
 run %w{./example_ts_transparent_clock.rb}
 run %w{./example_tsn_fp.rb}
 run %w{./example_tsn_frer.rb}
 run %w{./example_tsn_psfp.rb}
 run %w{./example_vop.rb}
-run %w{./example_mrp.rb}
-run %w{./example_port_conf_bulk.rb}
-run %w{./example_port_dynamic.rb}
 
