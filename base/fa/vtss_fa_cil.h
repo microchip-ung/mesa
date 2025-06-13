7588018673ab24208de52eb83ee14ea0527ad00a
Charles Perry <charles.perry@microchip.com>
2025-06-13 09:28:32 -0700

    cil: add RT_HSCH_SE_HIGH
    This is the index of first small SE being the highest inputs for
    large elements.

--- base/fa/vtss_fa_cil.h
+++ base/fa/vtss_fa_cil.h
@@ -73,6 +73,7 @@
 #define RT_TAS_NUMBER_OF_ENTRIES      (0x1FFF + 1)
 #define RT_HSCH_LAYERS                4
 #define RT_HSCH_L0_SES                1480
+#define RT_HSCH_SE_HIGH               1184
 #define RT_HSCH_L1_SES                37
 #define RT_HSCH_L2_SES                37
 #define RT_HSCH_L3_QSHPS              (1480 * 2)
@@ -113,6 +114,7 @@
 #define RT_TAS_NUMBER_OF_ENTRIES  (0x3FFF + 1)
 #define RT_HSCH_LAYERS            4
 #define RT_HSCH_L0_SES            5040
+#define RT_HSCH_SE_HIGH           4480
 #define RT_HSCH_L1_SES            64
 #define RT_HSCH_L2_SES            70
 #define RT_HSCH_L3_QSHPS          (5040 * 2)
@@ -160,6 +162,7 @@
 #define RT_TAS_NUMBER_OF_ENTRIES      (0x1FFF + 1)
 #define RT_HSCH_LAYERS                4
 #define RT_HSCH_L0_SES                1120
+#define RT_HSCH_SE_HIGH               1120
 #define RT_HSCH_L1_SES                32
 #define RT_HSCH_L2_SES                35
 #define RT_HSCH_L3_QSHPS              1120
