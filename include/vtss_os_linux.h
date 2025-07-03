d64abb6db8641b5fe7b620962afcd370be7bdd31
Henrik Bjoernlund <henrik.bjoernlund@microchip.com>
2025-07-03 15:38:25 +0200

    Fix of the VTSS_MSLEEP(msec) macro

--- include/vtss_os_linux.h
+++ include/vtss_os_linux.h
@@ -65,7 +65,7 @@ void vsim_nsleep(u32 n);
 extern void vtss_os_nsleep(u32 nsec);
 
 /** Sleep for \param msec milliseconds */
-#define VTSS_MSLEEP(msec) vtss_os_nsleep(msec)
+#define VTSS_MSLEEP(msec) vtss_os_msleep(msec)
 extern void vtss_os_msleep(u32 msec);
 #endif // _VTSS_MAIN_TYPES_H_
 #endif /* BOARD_SERVAL_VSIM */
